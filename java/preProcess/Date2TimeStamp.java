188bd39f0b78cd5048fe1909c585eaa342fdbb92
sidana <sidana@racer.imag.fr>
2017-01-15 21:31:19 +0100

    add code for netflix and recsyschallenge

---
+++ java/preProcess/Date2TimeStamp.java
@@ -0,0 +1,12 @@
+package preProcess;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Date2TimeStamp {
+	
+	public static void main(String [] args) throws IOException, ParseException{
+		InputOutput.date2timeStamp(args[0], args[1]);
+	}
+
+}
--- java/preProcess/InputOutput.java
+++ java/preProcess/InputOutput.java
@@ -5,6 +5,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -18,6 +23,29 @@ public class InputOutput {
 				line = line.replace("mv_", "").replace(".txt", "");
 				line = StringUtils.stripStart(line,"0");
 				printWriterTrain.println(line);
+				// TODO Auto-generated method stub
+			}
+			printWriterTrain.close();
+			br.close();
+		}
+
+	}
+	
+	public static void date2timeStamp(String inputFile,String outputFile) throws IOException, ParseException{
+		PrintWriter printWriterTrain = new PrintWriter (outputFile);
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(inputFile)))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String [] array = line.split(",");
+				String date_Temp = array[array.length-1];
+				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+				java.util.Date utilDate = dateFormat.parse(date_Temp);	
+				Date date = new java.sql.Date(utilDate.getTime());	
+				long time = date.getTime();
+//				Timestamp timestamp = new Timestamp(time);
+				for(int i = 0 ; i < array.length -1 ; i++)
+					printWriterTrain.print(array[i]+",");
+				printWriterTrain.println(time);
 				// TODO Auto-generated method stub
 			}
 			printWriterTrain.close();
