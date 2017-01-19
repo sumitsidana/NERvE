c89a0ea37d5479f01329fbdf508d62a7217ebc42
sidana <sidana@tiger.imag.fr>
2017-01-19 18:25:16 +0100

    java/preprocess.sh

--- java/preprocess.sh
+++ java/preprocess.sh
@@ -1,5 +1,5 @@
-sed -i.bak 1i"[userid_clicks,offerid_clicks,userid_offers,offerid_offers]" totalData.csv
+# sed -i.bak 1i"[userid_clicks,offerid_clicks,userid_offers,offerid_offers]" totalData.csv
 header=`echo -e "userid\tofferid\trating"`
-javac /home/ama/sidana/nnmf_ranking/java/preProcess/RemoveBrackets.java /home/ama/sidana/nnmf_ranking/java/preProcess/InputOutput.java /home/ama/sidana/nnmf_ranking/java/preProcess/CreateDelimiterSeperatedInput.java
+javac -cp binaries/commons-lang3-3.5.jar  preProcess/*.java
 java -cp /home/ama/sidana/nnmf_ranking/java preProcess.RemoveBrackets /data/sidana/nnmf_ranking/recsys2016_click/totalData.csv /data/sidana/nnmf_ranking/recsys2016_click/totalDataRanking.csv
 java -cp /home/ama/sidana/nnmf_ranking/java preProcess.CreateDelimiterSeperatedInput /data/sidana/nnmf_ranking/recsys2016_click/totalDataRanking.csv /data/sidana/nnmf_ranking/recsys2016_click/recsys.challenge.input "$header"
