b236bdfa1b9d2d4c447ae8e46e6c3b4343e2c79d
sidana <sidana@racer.imag.fr>
2017-01-19 12:18:57 +0100

    change path of preprocessing script

--- java/preprocess.sh
+++ java/preprocess.sh
@@ -1,5 +1,5 @@
-sed -i.bak 1i"[userid_clicks,offerid_clicks,userid_offers,offerid_offers]" /data/sidana/nnmf_ranking/rschallenge2016/totalData.csv
+sed -i.bak 1i"[userid_clicks,offerid_clicks,userid_offers,offerid_offers]" totalData.csv
 header=`echo -e "userid\tofferid\trating"`
-javac /home/ama/sidana/recsysBaselines/code/java/preProcess/RemoveBrackets.java /home/ama/sidana/recsysBaselines/code/java/preProcess/InputOutput.java /home/ama/sidana/recsysBaselines/code/java/preProcess/CreateDelimiterSeperatedInput.java
-java -cp /home/ama/sidana/recsysBaselines/code/java preProcess.RemoveBrackets /data/sidana/nnmf_ranking/rschallenge2016/totalData.csv /data/sidana/nnmf_ranking/rschallenge2016/totalDataRanking.csv
-java -cp /home/ama/sidana/recsysBaselines/code/java preProcess.CreateDelimiterSeperatedInput /data/sidana/nnmf_ranking/rschallenge2016/totalDataRanking.csv /data/sidana/nnmf_ranking/rschallenge2016/recsys.challenge.input "$header"
+javac /home/ama/sidana/nnmf_ranking/java/preProcess/RemoveBrackets.java /home/ama/sidana/nnmf_ranking/java/preProcess/InputOutput.java /home/ama/sidana/nnmf_ranking/java/preProcess/CreateDelimiterSeperatedInput.java
+java -cp /home/ama/sidana/nnmf_ranking/java preProcess.RemoveBrackets /data/sidana/nnmf_ranking/recsys2016_click/totalData.csv /data/sidana/nnmf_ranking/recsys2016_click/totalDataRanking.csv
+java -cp /home/ama/sidana/nnmf_ranking/java preProcess.CreateDelimiterSeperatedInput /data/sidana/nnmf_ranking/recsys2016_click/totalDataRanking.csv /data/sidana/nnmf_ranking/recsys2016_click/recsys.challenge.input "$header"
