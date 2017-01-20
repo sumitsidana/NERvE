98db8a99b5d4a55b59cd0d9c35d6e544899db126
sumit <sumit.sidana@imag.fr>
2017-01-20 17:06:54 +0100

    code: netflix param tune

--- netflix_bpr_nn_param_tune.py
+++ netflix_bpr_nn_param_tune.py
@@ -84,78 +84,57 @@ def inner_network(user_emb, item_emb):
     net = slim.fully_connected(inputs=net, num_outputs=1, activation_fn=None)
     return net
 
-
+f = open('/data/sidana/nnmf_ranking/netflix/param_tune', 'w')
 # In[29]:
-
-model = bprnn.BPR_NN(N_USERS, N_ITEMS, N_EMBEDDINGS, alpha=0.5, beta=0.5, alpha_reg=0.0, inner_net=inner_network)
-model.build_graph()
-model.initialize_session()
-
-
-# In[30]:
-
-losses = []
-batch_size = 512
-for n_batches, cur_optim in [(10000, model.trainer_3)]:
-    for i in tqdm(range(n_batches)):
-        batch = ds.sample_train_batch(n_samples=batch_size)
+for i in np.arange(0, 1, 0.1):
+    b=1-i
+    print ('alpha: '+ i + 'beta: '+ b)
+    model = bprnn.BPR_NN(N_USERS, N_ITEMS, N_EMBEDDINGS, alpha=i, beta=b, alpha_reg=0.0, inner_net=inner_network)
+    model.build_graph()
+    model.initialize_session()
+    losses = []
+    batch_size = 512
+    for n_batches, cur_optim in [(10000, model.trainer_3)]:
+        for i in tqdm(range(n_batches)):
+            batch = ds.sample_train_batch(n_samples=batch_size)
+            fd = {
+                model.user_ids:  batch['users'],
+                model.left_ids:  batch['left_items'],
+                model.right_ids: batch['right_items'],
+                model.target_y:  batch['y'],
+            }
+            el, nl, reg, t, _ = model.session.run(
+                [model.embedding_loss, model.net_loss, model.regularization, model.target, cur_optim],
+                feed_dict=fd
+            )
+            losses.append((el, nl, reg, t))
+            if i%500==0:
+                user_norm = np.linalg.norm(model.weights_u)
+                item_norm = np.linalg.norm(model.weights_i)
+                print('[it {}] weight norms, users: {}, items: {}'.format(i, user_norm, item_norm))
+                print('[it {}] metrics (emb_loss, net_loss, reg, target): {}'.format(i, losses[-1]))
+    ndcg_vals = []
+    for u in tqdm(ds.data_keys, desc='Prediction', leave=True):
+        response = np.zeros(len(ds.test[u]))
         fd = {
-            model.user_ids:  batch['users'], 
-            model.left_ids:  batch['left_items'],
-            model.right_ids: batch['right_items'],
-            model.target_y:  batch['y'],
-        }
-        el, nl, reg, t, _ = model.session.run(
-            [model.embedding_loss, model.net_loss, model.regularization, model.target, cur_optim], 
-            feed_dict=fd
-        )
-        losses.append((el, nl, reg, t))
-        if i%500==0:
-            user_norm = np.linalg.norm(model.weights_u)
-            item_norm = np.linalg.norm(model.weights_i)
-            print('[it {}] weight norms, users: {}, items: {}'.format(i, user_norm, item_norm))
-            print('[it {}] metrics (emb_loss, net_loss, reg, target): {}'.format(i, losses[-1]))
+                model.user_ids:  (np.ones(len(ds.test[u]))*u).astype(np.int32),
+                model.left_ids:  np.array([i for (i, r) in ds.test[u]]).astype(np.int32),
+            }
+        response += model.session.run(model.embedding_left, feed_dict=fd)[:, 0]
+        response += model.session.run(model.left_output, feed_dict=fd)[:, 0]
 
-
-# In[53]:
-
-# plot([x[0] for x in losses], c='b', label='embedding_loss', alpha=0.5)
-# plot([x[1] for x in losses], c='m', label='net_loss', alpha=0.5)
-# plot([x[2]*model.alpha_reg for x in losses], c='r', label='reg*alpha', alpha=0.5)
-# plot([x[3] for x in losses], c='g', label='target', alpha=0.5)
-# grid()
-# legend()
-# xlabel('n_batches')
-# ylabel('logloss')
-
-
-# In[31]:
-
-ndcg_vals = []
-for u in tqdm(ds.data_keys, desc='Prediction', leave=True):
-    response = np.zeros(len(ds.test[u]))
-    fd = {
-            model.user_ids:  (np.ones(len(ds.test[u]))*u).astype(np.int32), 
-            model.left_ids:  np.array([i for (i, r) in ds.test[u]]).astype(np.int32),
-        }
-    response += model.session.run(model.embedding_left, feed_dict=fd)[:, 0]
-    response += model.session.run(model.left_output, feed_dict=fd)[:, 0]
-
-    # make relevances
-    relevances = np.array([r for (i, r) in ds.test[u]])
-    predicted_ranking = np.argsort(-response)
-    # calc score
-    gain = letor_metrics.ndcg_from_ranking(relevances, predicted_ranking, 10)
-    ndcg_vals.append(gain)
-
-
-# In[32]:
-
-print (np.mean(ndcg_vals))
-print (np.std(ndcg_vals))
-
-
-# In[ ]:
+        # make relevances
+        relevances = np.array([r for (i, r) in ds.test[u]])
+        predicted_ranking = np.argsort(-response)
+        # calc score
+        gain = letor_metrics.ndcg_from_ranking(relevances, predicted_ranking, 10)
+        ndcg_vals.append(gain)
+    print (np.mean(ndcg_vals))
+    mean_ndcg=np.mean(ndcg_vals)
+    print (np.std(ndcg_vals))
+    std_ndcg=np.std(ndcg_vals)
+    f.write('alpha:'+i+',beta:'+b+',ndcg:'+mean_ndcg+',std:'+std_ndcg)
+f.close()
 
 
 
