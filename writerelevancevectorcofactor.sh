7f1c86f7de25396e6a4edafb240f49878d31fe50
sumit <sumit.sidana@imag.fr>
2017-04-10 20:04:30 +0200

    add: eval metrics

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -5,4 +5,4 @@ echo 'making relevance vector'
 java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/rv/relevanceVector_outbrain 10
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len3.py /data/sidana/nnmf_ranking/archive_version/outbrain/ outbrain
+python3 compOfflineEvalMetrics_len2.py /data/sidana/nnmf_ranking/archive_version/outbrain/ outbrain
