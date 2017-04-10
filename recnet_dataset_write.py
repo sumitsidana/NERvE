4d8fe46ac0274b4e6685282d273211bb2ddab5ab
sumitsidana <sidana.sumit@gmail.com>
2017-04-10 23:43:52 +0200

    add: recnet dataset write

--- recnet_dataset_write.py
+++ recnet_dataset_write.py
@@ -1,151 +1,171 @@
+
+# coding: utf-8
+
+# In[19]:
+
+#get_ipython().magic(u'pylab inline')
+import random
+
 import numpy as np
 import random
+import pickle
 from tqdm import tqdm
+import letor_metrics
+import pyximport
+pyximport.install()
 
-import cachedrandom
+import matplotlib
+# In[20]:
 
-
-class TripletsDataset(object):
-    """
-    Class for handling triplets data (u, i, r).
-    """
-    def __init__(self, raw_data, threshold_user=20, threshold_item=0, rnd_seed=42):
-
-        self.threshold_user = threshold_user
-        self.threshold_item = threshold_item
-
-        self.statistics = {}
-
-        # fix randomness
-        self.seed = rnd_seed
-        self.set_random_seed(rnd_seed)
-
-        # calculate item counts (and move to blacklist if less than threshold_item)
-        i_cnt = {}
-        for row in tqdm(raw_data, desc='Calculate items counts', leave=False):
-            u, i, r, t = row.astype(int)
-            i_cnt[i] = i_cnt.get(i, 0) + 1
-        i_blacklist = set([k for k, v in i_cnt.items() if v < threshold_item])
-        self.statistics['items'] = set([k for k, v in i_cnt.items() if v >= threshold_item])
-
-        # calculate item counts (and move to blacklist if less than threshold_user)
-        u_cnt = {}
-        for row in tqdm(raw_data, desc='Calculate users counts', leave=False):
-            u, i, r, t = row.astype(int)
-            if i not in i_blacklist:
-                u_cnt[u] = u_cnt.get(u, 0) + 1
-        u_blacklist = set([k for k, v in u_cnt.items() if v < threshold_user])
-        self.statistics['users'] = set([k for k, v in u_cnt.items() if v >= threshold_user])
+raw_data = np.loadtxt('/data/sidana/nnmf_ranking/outbrainchallenge/dat.outbrainchallenge.withindexedusers.withreplacedrating', delimiter=',')
 
 
-        # assemble self.data like {user:[(item, rating),...], ...}
-        self.data = {}
-        self.statistics['cnt_total'] = 0
-        for row in tqdm(raw_data, desc='Assemble .data', leave=False):
-            u, i, r, t = list(map(int, row))
-            # filtering blaclisted elements
-            if (u in u_blacklist) or (i in i_blacklist):
-                continue
-            self.data[u] = self.data.get(u, []) + [(i, r)]
-            self.statistics['cnt_total'] += 1
-        self.data_keys = list(self.data.keys())
+# In[21]:
 
-        self.init_cached_random()
+from dataset import TripletsDataset
 
 
-    def set_random_seed(self, seed):
-        # fix randomness
-        np.random.seed(seed)
-        random.seed(seed)
+# In[22]:
 
-    def init_cached_random(self):
-        # init cached randomizers
-        self.pair_sampler = cachedrandom.CachedSampler2from5(blob_size=10000000)
-        self.data_keys_sampler = cachedrandom.CachedRandomizer(high=len(self.data_keys), blob_size=10000000)
+ds = TripletsDataset(raw_data, threshold_user=60, rnd_seed=42)
+ds.train_test_split(n_train=50)
 
 
+# In[5]:
+
+# pickle.dump(ds, open('./tmp/ds.pkl', 'wb'))
+# ds = pickle.load(open('./tmp/ds_50_mf.pkl', 'rb'))
 
 
-    def train_test_split(self, n_train, seed=None):
-        if seed is None:
-            print('Warning: random seed is None, default class seed ({}) will be used'.format(self.seed))
-            seed = self.seed
-        self.set_random_seed(seed)
+# In[23]:
 
-        # .train : {user: {rating: [items, ...], ...}}
-        self.train = {}
-        self.statistics['cnt_train'] = 0
-
-        # .test : {user: [(item, rating), ..]} [sorted by rating]
-        self.test = {}
-        self.statistics['cnt_test'] = 0
-
-        for u in tqdm(self.data, desc='Split users', leave=False):
-            # select random indicies which will be used for training for current user u
-            train_inds = set(random.sample(range(len(self.data[u])), n_train))
-            for n, (i, r) in enumerate(self.data[u]):
-                if n in train_inds:
-                    # move to train
-                    user_dict = self.train.get(u, {})
-                    rating_list = user_dict.get(r, [])
-                    rating_list.append(i)
-                    user_dict[r] = rating_list
-                    self.train[u] = user_dict
-                    self.statistics['cnt_train'] += 1
-                else:
-                    # move to test
-                    self.test[u] = self.test.get(u, []) + [(i, r)]
-                    self.statistics['cnt_test'] += 1
-
-            # ad-hoc for situation with all equal ratings in train
-            if len(self.train[u].keys()) == 1:
-                print('No rating diversity in train set for user {}, do swap!'.format(u))
-                the_only_rating = list(self.train[u].keys())[0]
-                for n, (i, r) in enumerate(self.test[u]):
-                    # find first different rating in test and swap it with 0-th in train
-                    if r != the_only_rating:
-                        self.train[u][r] = [i]
-                        extracted_i = self.train[u][the_only_rating][0]
-                        self.train[u][the_only_rating] = self.train[u][the_only_rating][1:]
-                        del self.test[u][n]
-                        self.test[u] = self.test[u] + [(extracted_i, the_only_rating)]
-                        break
-            self.test[u] = sorted(self.test[u], key=lambda x: x[1], reverse=True)
-
-        # intersect train and test items
-        self.used_in_trainset = set()
-        for u in self.train:
-            for r in self.train[u]:
-                self.used_in_trainset.update(self.train[u][r])
-        for u in self.test:
-            self.test[u] = [(i, r) for (i, r) in self.test[u] if i in self.used_in_trainset]
+ds.init_cached_random()
 
 
-    def sample_train_triple(self):
+# # define model
 
-        user = self.data_keys[self.data_keys_sampler.sample()]
-        stats = self.train[user]
-        stats_keys = list(stats.keys())
-        assert len(stats_keys) > 1, 'user {} has only 1 rating!'.format(user)
+# In[24]:
 
-        left_rating, right_rating = self.pair_sampler.sample(stats_keys)
-        assert left_rating != right_rating
-
-        left_value = random.choice(stats[left_rating])
-        right_value = random.choice(stats[right_rating])
-        y = (left_rating > right_rating)*2 - 1
-        diff = left_rating - right_rating
-        return (user, left_value, right_value, y, diff)
+import tensorflow as tf
+import bprnn
 
 
-    def sample_train_batch(self,n_samples=256):
-        retval = np.zeros((n_samples, 5)).astype(np.int32)
-        for i in range(n_samples):
-            retval[i] = self.sample_train_triple()
-        return {
-            'users': retval[:, 0],
-            'left_items': retval[:, 1],
-            'right_items': retval[:, 2],
-            'y': retval[:, 3].astype(np.float32),
-            'diff': retval[:, 4].astype(np.float32),
+# In[25]:
+
+import imp
+
+
+# In[26]:
+
+imp.reload(bprnn)
+
+
+# In[48]:
+
+# model.destroy()
+
+
+# In[27]:
+
+N_USERS = 49616 #int(max(raw_data[:, 0])) + 1
+N_ITEMS = 547865 #int(max(raw_data[:, 1])) + 1
+N_EMBEDDINGS = 1
+
+
+# In[28]:
+
+import tensorflow.contrib.slim as slim
+
+def inner_network(user_emb, item_emb):
+    joined_input = tf.concat(1, [user_emb, item_emb])
+    net = slim.fully_connected(inputs=joined_input, num_outputs=64, activation_fn=tf.nn.relu)
+#     net = slim.fully_connected(inputs=joined_input, num_outputs=64, activation_fn=tf.nn.relu)
+#     net = slim.dro
+    net = slim.fully_connected(inputs=net, num_outputs=1, activation_fn=None)
+    return net
+
+
+# In[29]:
+
+model = bprnn.BPR_NN(N_USERS, N_ITEMS, N_EMBEDDINGS, alpha=0.0, beta=1.0, alpha_reg=0.0, inner_net=inner_network)
+model.build_graph()
+model.initialize_session()
+
+
+# In[30]:
+
+losses = []
+batch_size = 512
+for n_batches, cur_optim in [(10000, model.trainer_3)]:
+    for i in tqdm(range(n_batches)):
+        batch = ds.sample_train_batch(n_samples=batch_size)
+        fd = {
+            model.user_ids:  batch['users'], 
+            model.left_ids:  batch['left_items'],
+            model.right_ids: batch['right_items'],
+            model.target_y:  batch['y'],
         }
+        el, nl, reg, t, _ = model.session.run(
+            [model.embedding_loss, model.net_loss, model.regularization, model.target, cur_optim], 
+            feed_dict=fd
+        )
+        losses.append((el, nl, reg, t))
+        if i%500==0:
+            user_norm = np.linalg.norm(model.weights_u)
+            item_norm = np.linalg.norm(model.weights_i)
+            print('[it {}] weight norms, users: {}, items: {}'.format(i, user_norm, item_norm))
+            print('[it {}] metrics (emb_loss, net_loss, reg, target): {}'.format(i, losses[-1]))
+
+
+# In[53]:
+
+# plot([x[0] for x in losses], c='b', label='embedding_loss', alpha=0.5)
+# plot([x[1] for x in losses], c='m', label='net_loss', alpha=0.5)
+# plot([x[2]*model.alpha_reg for x in losses], c='r', label='reg*alpha', alpha=0.5)
+# plot([x[3] for x in losses], c='g', label='target', alpha=0.5)
+# grid()
+# legend()
+# xlabel('n_batches')
+# ylabel('logloss')
+
+
+# In[31]:
+
+export_basename = '/data/sidana/nnmf_ranking/outbrainchallenge/vectors/'
+export_pred = open(export_basename + 'pr_5_01', 'w')
+export_true = open(export_basename + 'gt_5_01', 'w')
+
+ndcg_vals = []
+for u in tqdm(ds.data_keys, desc='Prediction', leave=True):
+    response = np.zeros(len(ds.test[u]))
+    fd = {
+            model.user_ids:  (np.ones(len(ds.test[u]))*u).astype(np.int32),
+            model.left_ids:  np.array([i for (i, r) in ds.test[u]]).astype(np.int32),
+        }
+    response += model.session.run(model.embedding_left, feed_dict=fd)[:, 0]
+    response += model.session.run(model.left_output, feed_dict=fd)[:, 0]
+
+    # make relevances
+    relevances = np.array([r for (i, r) in ds.test[u]])
+    items = np.array([i for (i, r) in ds.test[u]])  # it's already sorted by true relevance
+    itemsGroundTruth = np.array([i for (i,r) in ds.test[u] if r == 4])
+    predicted_ranking = np.argsort(-response)
+
+    # write down predictions
+    export_pred.write(' '.join(map(str, [u] + list(items[predicted_ranking]))) + '\n')
+    export_true.write(' '.join(map(str, [u] + list(itemsGroundTruth))) + '\n')
+
+    # calc score
+    gain = letor_metrics.ndcg_from_ranking(relevances, predicted_ranking, 5)
+    ndcg_vals.append(gain)
+
+
+# In[32]:
+
+print (np.mean(ndcg_vals))
+
+
+# In[ ]:
+
+
+
