ccfc125a9311ca74f89a53833886eea1deb5fa1b
sumit <sumit.sidana@imag.fr>
2017-04-10 19:57:09 +0200

    add: rel vec

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -1,11 +1,8 @@
 LANG=en_US.utf8
-for i in "10" "20" "30" "50" "75" "100"
-do
-    cd java/src
-    echo 'making relevance vector'
-    javac -cp ../binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
-    java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/adaptivity/baseline_algorithms/cofactor/outbrain/len$i/vectors/gt_outbrain /data/sidana/adaptivity/baseline_algorithms/cofactor/outbrain/len$i/vectors/pr_outbrain /data/sidana/adaptivity/baseline_algorithms/cofactor/outbrain/len$i/one/rv/relevanceVector_outbrain 2
-    cd -
-    echo 'compute offline metrics'
-    python3 compOfflineEvalMetrics_len3.py /data/sidana/adaptivity/baseline_algorithms/cofactor/outbrain/len$i/one/ outbrain
-done
+cd java/src
+echo 'making relevance vector'
+javac -cp ../binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/rv/relevanceVector_outbrain 10
+cd -
+echo 'compute offline metrics'
+python3 compOfflineEvalMetrics_len3.py /data/sidana/nnmf_ranking/archive_version/outbrain/ outbrain
