7bdd9da81dad645eca9ac9e12a10cc0ed48bebc2
sumit <sumit.sidana@imag.fr>
2017-04-10 20:03:36 +0200

    add: eval metrics

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -1,6 +1,6 @@
 LANG=en_US.utf8
 cd java/
-javac -cp ../binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
+javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
 java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/rv/relevanceVector_outbrain 10
 cd -
