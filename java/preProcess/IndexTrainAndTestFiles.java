4ec2c042269624f7ab82f8a5890764cfbe6ad3f8
sumit <sumit.sidana@imag.fr>
2017-04-11 09:17:40 +0200

    add: java class for indexing train and test together

---
+++ java/preProcess/IndexTrainAndTestFiles.java
@@ -0,0 +1,11 @@
+package preProcess;
+
+import java.io.IOException;
+
+public class IndexTrainAndTestFiles {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		InputOutput.indexTrainAndTestFiles(args[0], args[1], args[2], args[3], args[4], args[5]);
+	}
+}
