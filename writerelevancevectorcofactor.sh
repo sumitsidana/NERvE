8697a9947d568029c5d9e7b653e9c8c6f9ad07a9
sumit <sumit.sidana@imag.fr>
2017-04-11 09:39:36 +0200

    add last thing

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -2,7 +2,7 @@ LANG=en_US.utf8
 cd java/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/rv/relevanceVector_outbrain 10
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/five/rv/relevanceVector_outbrain 6
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len2.py /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/ outbrain
+python3 compOfflineEvalMetrics_len4.py /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/five outbrain
