c4c95b6cebf08ef9987f56511a87d59644dbd085
sidana <sidana@tiger.imag.fr>
2017-04-28 13:42:11 +0200

    add cofactor for outbrain

--- cofactor_outbrain.sh
+++ cofactor_outbrain.sh
@@ -2,5 +2,5 @@ cd ~/cofactor/src
 LANG=en_US.utf8
 echo "running for length: $i"
 echo "preprocessing"
-#python preprocess_ML1M.py "/data/sidana/nnmf_ranking/archive_version/outbrain"
+python preprocess_ML1M.py "/data/sidana/nnmf_ranking/archive_version/outbrain"
 python Cofactorization_outbrain.py "/data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/pro"
