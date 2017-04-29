b39eeb05d00de055b21622b46598b908760bbb72
sumitsidana <sidana.sumit@gmail.com>
2017-04-29 10:56:39 +0200

    add: write gt for cofactor

--- writegroundtruthforcofactor.sh
+++ writegroundtruthforcofactor.sh
@@ -1,6 +1,6 @@
 javac -cp ~/embAdaptivity/java/binaries/commons-lang3-3.5.jar  ~/embAdaptivity/java/src/preProcess/InputOutput.java ~/embAdaptivity/java/src/preProcess/WriteGroundTruthForCofactor.java
 LANG=en_US.utf8
-mkdir -p  /data/sidana/nnmf_ranking/archive_version/outbrain/vectors
+mkdir -p  /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors
 echo "running for length: $i"
 echo "preprocessing"
 java -cp ~/embAdaptivity/java/src/ preProcess.WriteGroundTruthForCofactor  "/data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/pro/test.csv" "/data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/gt_outbrain"
