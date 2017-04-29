503fd0b768a9619eaae87a6d8266edbdff8557ff
sumitsidana <sidana.sumit@gmail.com>
2017-04-29 11:26:59 +0200

    add: relevance vector code

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -1,6 +1,7 @@
 LANG=en_US.utf8
 cd java/
 mkdir -p /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/rv/
+mkdir -p /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
 java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor//rv/relevanceVector_outbrain 10
