8a59d0e5ec0e0c8846a3485d73170187658016d1
sumitsidana <sidana.sumit@gmail.com>
2017-05-01 01:00:06 +0200

    correct relevance vector

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -4,7 +4,7 @@ mkdir -p /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/rv/
 mkdir -p /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/gt_ml20m /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/pr_ml20m /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/rv/relevanceVector_ml20m 5
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/gt_ml20m /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/pr_ml20m /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/rv/relevanceVector_ml20m 10
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len3.py /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/ ml20m
+python3 compOfflineEvalMetrics_len2.py /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/ ml20m
