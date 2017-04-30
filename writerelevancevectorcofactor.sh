006dfe06169afc46b549f15774a4c7ed901770ee
sumitsidana <sidana.sumit@gmail.com>
2017-05-01 01:02:51 +0200

    ml20m->outbrain

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -1,10 +1,10 @@
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/five/rv/
-mkdir -p /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/five/em/
+mkdir -p /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/five/rv/
+mkdir -p /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/five/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/gt_ml20m /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/pr_ml20m /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/five/rv/relevanceVector_ml20m 5
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/gt_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/pr_outbrain /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/five/rv/relevanceVector_outbrain 5
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len4.py /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/five ml20m
+python3 compOfflineEvalMetrics_len4.py /data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/five outbrain
