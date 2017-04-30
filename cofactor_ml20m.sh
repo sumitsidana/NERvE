003e23821d59712ee40ecf3b036b84540b6c7f88
sumitsidana <sidana.sumit@gmail.com>
2017-04-30 22:13:31 +0200

    add cofactor for ml20m;

--- cofactor_ml20m.sh
+++ cofactor_ml20m.sh
@@ -2,8 +2,8 @@ cd ~/cofactor/src
 LANG=en_US.utf8
 echo "running for length: $i"
 echo "preprocessing"
-python preprocess_ml20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor"
-python Cofactorization_ml20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro"
+python preprocess_ML20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor"
+python Cofactorization_ML20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro"
 ./writegroundtruthforcofactor.sh
 ./writepredictorforcofator.sh
 ./writerelevancevectorcofactor.sh
