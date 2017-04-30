961af899acc0e7156b0e221bc6684631de8ec381
sidana <sidana@tiger.imag.fr>
2017-05-01 00:40:31 +0200

    add: shell script for cofactor

--- cofactor_ml20m.sh
+++ cofactor_ml20m.sh
@@ -4,6 +4,7 @@ echo "running for length: $i"
 echo "preprocessing"
 python preprocess_ml20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor"
 python Cofactorization_ml20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro"
+cd -
 ./writegroundtruthforcofactor.sh
 ./writepredictorforcofator.sh
 ./writerelevancevectorcofactor.sh
