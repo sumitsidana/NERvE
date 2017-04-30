70645898b9a187bee4c25efaab67945eba73cac5
sumitsidana <sidana.sumit@gmail.com>
2017-04-30 21:56:01 +0200

    add: shell scripts for cofactor

---
+++ cofactor_ml20m.sh
@@ -0,0 +1,6 @@
+cd ~/cofactor/src
+LANG=en_US.utf8
+echo "running for length: $i"
+echo "preprocessing"
+python preprocess_ml20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor"
+python Cofactorization_ml20m.py "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro"
--- writepredictorforcofator.sh
+++ writepredictorforcofator.sh
@@ -2,4 +2,4 @@ javac -cp ~/embAdaptivity/java/binaries/commons-lang3-3.5.jar  ~/embAdaptivity/j
 LANG=en_US.utf8
 echo "running for length: $i"
 echo "preprocessing"
-java -cp ~/embAdaptivity/java/src/ preProcess.WritePredictorForCofactor "/data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/pro/test_all.csv" "/data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/pro/pr" "/data/sidana/nnmf_ranking/archive_version/outbrain/cofactor/vectors/pr_outbrain"
+java -cp ~/embAdaptivity/java/src/ preProcess.WritePredictorForCofactor "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro/test_all.csv" "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro/pr" "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/pr_ml20m"
