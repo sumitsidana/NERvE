38a32074a8bdaf2647f57f891d6b8f992ddf3521
sumit <sumit.sidana@imag.fr>
2017-05-02 12:09:04 +0200

    add: ground truth

--- writepredictorforbprmf.sh
+++ writepredictorforbprmf.sh
@@ -3,4 +3,4 @@ LANG=en_US.utf8
 mkdir -p  /data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/vectors
 echo "running for length: $i"
 echo "preprocessing"
-java -cp ~/nnmf_ranking/java/src/ preProcess.WritePredictorForBPRMF "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/test.bprmf" "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/prediction_file" "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/vectors/pr_outbrain"
+java -cp ~/nnmf_ranking/java/ preProcess.WritePredictorForBPRMF "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/test.bprmf" "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/prediction_file" "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/vectors/pr_outbrain"
