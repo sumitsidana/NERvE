c21725d6fe5632067a1c25577747196f7514fee6
sumit <sumit.sidana@imag.fr>
2017-05-02 11:58:30 +0200

    add: ground truth

--- writegroundtruthforbprmf.sh
+++ writegroundtruthforbprmf.sh
@@ -1,6 +1,6 @@
-javac -cp ~/nnmf_ranking/java/src/binaries/commons-lang3-3.5.jar  ~/nnmf_ranking/java/src/preProcess/InputOutput.java ~/nnmf_ranking/java/src/preProcess/WriteGroundTruthForbprmf.java
+javac -cp ~/nnmf_ranking/java/binaries/commons-lang3-3.5.jar  ~/nnmf_ranking/java/preProcess/InputOutput.java ~/nnmf_ranking/java/preProcess/WriteGroundTruthForbprmf.java
 LANG=en_US.utf8
 mkdir -p  /data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/vectors
 echo "running for length: $i"
 echo "preprocessing"
-java -cp ~/nnmf_ranking/java/src/ preProcess.WriteGroundTruthForbprmf  "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/test.bprmf" "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/vectors/gt_outbrain"
+java -cp ~/nnmf_ranking/java/ preProcess.WriteGroundTruthForbprmf  "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/test.bprmf" "/data/sidana/nnmf_ranking/archive_version/outbrain/bprmf/vectors/gt_outbrain"
