3f4b5d61867d4e66b84795e25aeaff3da022056e
LLOleg <o.gorodnitskii@skoltech.ru>
2017-05-08 19:27:01 +0300

    Add files via upload
    dataset_tt_static - dataset file for work with static train/test files
    train_test_split - example

---
+++ dataset_tt_static.py
@@ -0,0 +1,198 @@
+import numpy as np
+import random
+from tqdm import tqdm
+
+import cachedrandom
+
+
+class TripletsDataset(object):
+    """
+    Class for handling triplets data (u, i, r).
+    """
+    def __init__(self, raw_data_train, raw_data_test, binarize = 1, threshold_user=20, threshold_item=0, rnd_seed=42):
+
+        self.threshold_user = threshold_user
+        self.threshold_item = threshold_item
+        self.train_len = len(raw_data_train)
+        self.test_len = len(raw_data_test)
+
+        self.statistics = {}
+
+        # fix randomness
+        self.seed = rnd_seed
+        self.set_random_seed(rnd_seed)
+
+        raw_data = np.concatenate((raw_data_train, raw_data_test))
+
+        # calculate item counts (and move to blacklist if less than threshold_item)
+        i_cnt = {}
+        for row in tqdm(raw_data, desc='Calculate items counts', leave=False):
+            u, i, r, t = row.astype(int)
+            if binarize == 1:
+                if r < 4:
+                    r = 0
+                else:
+                    r = 1
+                    
+            i_cnt[i] = i_cnt.get(i, 0) + 1
+        i_blacklist = set([k for k, v in i_cnt.items() if v < threshold_item])
+        self.statistics['items'] = set([k for k, v in i_cnt.items() if v >= threshold_item])
+
+        # calculate item counts (and move to blacklist if less than threshold_user)
+        u_cnt = {}
+        for row in tqdm(raw_data, desc='Calculate users counts', leave=False):
+            u, i, r, t = row.astype(int)
+            
+            if binarize == 1:
+                if r < 4:
+                    r = 0
+                else:
+                    r = 1
+                    
+                    
+            if i not in i_blacklist:
+                u_cnt[u] = u_cnt.get(u, 0) + 1
+        u_blacklist = set([k for k, v in u_cnt.items() if v < threshold_user])
+        self.statistics['users'] = set([k for k, v in u_cnt.items() if v >= threshold_user])
+
+
+        # assemble self.data like {user:[(item, rating),...], ...}
+        self.data = {}
+        self.statistics['cnt_total'] = 0
+        ind = 0
+        
+        for row in tqdm(raw_data, desc='Assemble .data', leave=False):
+            u, i, r, t = list(map(int, row))
+            
+            if binarize == 1:
+                if r < 4:
+                    r = 0
+                else:
+                    r = 1
+                    
+            # filtering blaclisted elements
+            if (u in u_blacklist) or (i in i_blacklist):
+                ind += 1
+                continue
+            
+            self.data[u] = self.data.get(u, []) + [(i, r, ind)]
+            self.statistics['cnt_total'] += 1
+            ind += 1
+            
+        self.data_keys = list(self.data.keys())
+
+        self.init_cached_random()
+
+
+    def set_random_seed(self, seed):
+        # fix randomness
+        np.random.seed(seed)
+        random.seed(seed)
+
+    def init_cached_random(self):
+        # init cached randomizers
+        self.pair_sampler = cachedrandom.CachedSampler2from5(blob_size=10000000)
+        self.data_keys_sampler = cachedrandom.CachedRandomizer(high=len(self.data_keys), blob_size=10000000)
+
+
+
+
+    def train_test_split(self, seed=None):
+	
+        if seed is None:
+            print('Warning: random seed is None, default class seed ({}) will be used'.format(self.seed))
+            seed = self.seed
+        self.set_random_seed(seed)
+
+        n_train = np.copy(self.train_len)
+
+        # .train : {user: {rating: [items, ...], ...}}
+        self.train = {}
+        self.statistics['cnt_train'] = 0
+
+        # .test : {user: [(item, rating), ..]} [sorted by rating]
+        self.test = {}
+        self.statistics['cnt_test'] = 0
+
+        for u in tqdm(self.data, desc='Split users', leave=False):
+            # select random indicies which will be used for training for current user u
+            #train_inds = set(random.sample(range(len(self.data[u])), n_train))
+            train_inds = set(range(self.train_len))
+           
+            for n, (i, r, ind) in enumerate(self.data[u]):
+                if ind in train_inds:
+                    # move to train
+                    user_dict = self.train.get(u, {})
+                    rating_list = user_dict.get(r, [])
+                    rating_list.append(i)
+                    user_dict[r] = rating_list
+                    self.train[u] = user_dict
+                    self.statistics['cnt_train'] += 1
+                else:
+                    # move to test
+                    self.test[u] = self.test.get(u, []) + [(i, r)]
+                    self.statistics['cnt_test'] += 1
+
+            # ad-hoc for situation with all equal ratings in train
+            if len(self.train[u].keys()) == 1:
+                print('No rating diversity in train set for user {}, do swap!'.format(u))
+                the_only_rating = list(self.train[u].keys())[0]
+                for n, (i, r) in enumerate(self.test[u]):
+                    # find first different rating in test and swap it with 0-th in train
+                    if r != the_only_rating:
+                        self.train[u][r] = [i]
+                        extracted_i = self.train[u][the_only_rating][0]
+                        self.train[u][the_only_rating] = self.train[u][the_only_rating][1:]
+                        del self.test[u][n]
+                        self.test[u] = self.test[u] + [(extracted_i, the_only_rating)]
+                        break
+            self.test[u] = sorted(self.test.get(u, []), key=lambda x: x[1], reverse=True)
+
+        # intersect train and test items
+        self.used_in_trainset = set()
+        for u in self.train:
+            for r in self.train[u]:
+                self.used_in_trainset.update(self.train[u][r])
+        for u in self.test:
+            self.test[u] = [(i, r) for (i, r) in self.test[u] if i in self.used_in_trainset]
+
+
+    def sample_train_triple(self):
+
+        user = self.data_keys[self.data_keys_sampler.sample()]
+        stats = self.train[user]
+        stats_keys = list(stats.keys())
+        #assert len(stats_keys) > 1, 'user {} has only 1 rating!'.format(user)
+        if len(stats_keys) < 2:
+            return [-1]
+
+        left_rating, right_rating = self.pair_sampler.sample(stats_keys)
+        assert left_rating != right_rating
+
+        left_value = random.choice(stats[left_rating])
+        right_value = random.choice(stats[right_rating])
+        y = (left_rating > right_rating)*2 - 1
+        diff = left_rating - right_rating
+        return (user, left_value, right_value, y, diff)
+
+
+    def sample_train_batch(self,n_samples=256):
+        t = 0
+        retval = np.zeros((n_samples, 5)).astype(np.int32)
+        for i in range(n_samples):
+            result = self.sample_train_triple()
+          
+            if result[0] == -1:
+                i -= 1
+            else:
+                retval[i] = result
+                t +=1 
+
+        np.resize(retval, (t, 5))
+        return {
+            'users': retval[:, 0],
+            'left_items': retval[:, 1],
+            'right_items': retval[:, 2],
+            'y': retval[:, 3].astype(np.float32),
+            'diff': retval[:, 4].astype(np.float32),
+        }
---
+++ train_test_split.py
@@ -0,0 +1,95 @@
+import random
+import numpy as np
+import random
+import pickle
+import letor_metrics
+import pyximport
+from tqdm import tqdm
+pyximport.install()
+import matplotlib
+
+
+raw_data_train = np.loadtxt('/home/thirtythird/NNMF/ml-100k/train_all_raw.csv', skiprows = 1, delimiter=',')
+raw_data_test = np.loadtxt('/home/thirtythird/NNMF/ml-100k/test_all_raw.csv', skiprows = 1, delimiter=',')
+raw_data = np.concatenate((raw_data_train, raw_data_test))
+from dataset_tt_static import TripletsDataset
+
+ds = TripletsDataset(raw_data_train, raw_data_test, threshold_user=20, rnd_seed=42)
+ds.train_test_split()
+
+ds.init_cached_random()
+import tensorflow as tf
+import bprnn
+import imp
+
+N_USERS = int(max(raw_data[:, 0])) + 1
+N_ITEMS = int(max(raw_data[:, 1])) + 1
+N_EMBEDDINGS = 1
+
+import tensorflow.contrib.slim as slim
+imp.reload(bprnn)
+
+#%%
+def inner_network(user_emb, item_emb):
+    joined_input = tf.concat([user_emb, item_emb], 1)
+    net = slim.fully_connected(inputs=joined_input, num_outputs=64, activation_fn=tf.nn.relu)
+#     net = slim.fully_connected(inputs=joined_input, num_outputs=64, activation_fn=tf.nn.relu)
+#     net = slim.dro
+    net = slim.fully_connected(inputs=net, num_outputs=1, activation_fn=None)
+    return net
+
+model = bprnn.BPR_NN(N_USERS, N_ITEMS, N_EMBEDDINGS, alpha=0.5, beta=0.5, alpha_reg=0.0, inner_net=inner_network)
+model.build_graph()
+model.initialize_session()
+
+losses = []
+batch_size = 512
+for n_batches, cur_optim in [(10000, model.trainer_3)]:
+    for i in tqdm(range(n_batches)):
+        batch = ds.sample_train_batch(n_samples=batch_size)
+        fd = {
+            model.user_ids:  batch['users'], 
+            model.left_ids:  batch['left_items'],
+            model.right_ids: batch['right_items'],
+            model.target_y:  batch['y'],
+        }
+        el, nl, reg, t, _ = model.session.run(
+            [model.embedding_loss, model.net_loss, model.regularization, model.target, cur_optim], 
+            feed_dict=fd
+        )
+        losses.append((el, nl, reg, t))
+        if i%500==0:
+            user_norm = np.linalg.norm(model.weights_u)
+            item_norm = np.linalg.norm(model.weights_i)
+            print('[it {}] weight norms, users: {}, items: {}'.format(i, user_norm, item_norm))
+            print('[it {}] metrics (emb_loss, net_loss, reg, target): {}'.format(i, losses[-1]))
+
+#%%
+            
+ndcg_vals = []
+for u in tqdm(ds.data_keys, desc='Prediction', leave=True):
+    response = np.zeros(len(ds.test[u]))
+    fd = {
+            model.user_ids:  (np.ones(len(ds.test[u]))*u).astype(np.int32), 
+            model.left_ids:  np.array([i for (i, r) in ds.test[u]]).astype(np.int32),
+        }
+    response += model.session.run(model.embedding_left, feed_dict=fd)[:, 0]
+    response += model.session.run(model.left_output, feed_dict=fd)[:, 0]
+
+    # make relevances
+    relevances = np.array([r for (i, r) in ds.test[u]])
+    predicted_ranking = np.argsort(-response)
+    # calc score
+    gain = letor_metrics.ndcg_from_ranking(relevances, predicted_ranking, 10)
+    ndcg_vals.append(gain)
+
+
+# In[32]:
+
+print (np.mean(ndcg_vals))
+
+
+# In[ ]:
+
+
+
