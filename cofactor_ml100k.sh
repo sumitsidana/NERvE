8462262a1bfa695f8043f3f2f664606989e54c69
sumitsidana <sidana.sumit@gmail.com>
2017-05-26 00:25:17 +0200

    add: cofactor for ml100k

--- cofactor_ml100k.sh
+++ cofactor_ml100k.sh
@@ -1,6 +1,6 @@
 cd ~/cofactor/src
 LANG=en_US.utf8
 echo "preprocessing"
-python preprocess_outbrain.py "/data/sidana/nnmf_ranking/recnet_draft/ml100k/cofactor"
+python preprocess_outbrain.py "/data/sidana/recnet_draft/ml100k/cofactor"
 echo "running cofactor"
-python Cofactorization_outbrain.py "/data/sidana/nnmf_ranking/recnet_draft/ml100k/cofactor/pro"
+python Cofactorization_outbrain.py "/data/sidana/recnet_draft/ml100k/cofactor/pro"
