4b8ccc2b9ae774cf590e8c1d56ea4a4385f8304d
sumitsidana <sidana.sumit@gmail.com>
2017-05-26 00:52:13 +0200

    add: write ground truth for cofactor

--- writegroundtruthforcofactor.sh
+++ writegroundtruthforcofactor.sh
@@ -1,6 +1,6 @@
 javac -cp ~/embAdaptivity/java/binaries/commons-lang3-3.5.jar  ~/embAdaptivity/java/src/preProcess/InputOutput.java ~/embAdaptivity/java/src/preProcess/WriteGroundTruthForCofactor.java
 LANG=en_US.utf8
-mkdir -p  /data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors
+mkdir -p "/data/sidana/recnet_draft/$1/cofactor/vectors"
 echo "running for length: $i"
 echo "preprocessing"
-java -cp ~/embAdaptivity/java/src/ preProcess.WriteGroundTruthForCofactor  "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/pro/test.csv" "/data/sidana/nnmf_ranking/archive_version/ml20m/cofactor/vectors/gt_ml20m"
+java -cp ~/embAdaptivity/java/src/ preProcess.WriteGroundTruthForCofactor  "/data/sidana/recnet_draft/$1/cofactor/pro/test.csv" "/data/sidana/recnet_draft/$1/cofactor/vectors/gt_ml20m"
