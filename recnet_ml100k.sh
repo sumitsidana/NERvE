8530620a51db0fa539815574662abdd1968efd48
sumit <sumit.sidana@imag.fr>
2017-05-26 18:03:14 +0200

    add: shellscripts for automation

--- recnet_ml100k.sh
+++ recnet_ml100k.sh
@@ -1,12 +1,12 @@
-python3 train_test_split.py 0 1
+python3 train_test_split.py ml100k 0 1
 /writerelevancevectorrecnet.sh ml100k 01 one
 /writerelevancevectorrecnet.sh ml100k 01 five
 /writerelevancevectorrecnet.sh ml100k 01 ten
-python3 train_test_split.py 1 0
+python3 train_test_split.py ml100k 1 0
 /writerelevancevectorrecnet.sh ml100k 10 one
 /writerelevancevectorrecnet.sh ml100k 10 five
 /writerelevancevectorrecnet.sh ml100k 10 ten
-python3 train_test_split.py 1 1
+python3 train_test_split.py ml100k 1 1
 /writerelevancevectorrecnet.sh ml100k 11 one
 /writerelevancevectorrecnet.sh ml100k 11 five
 /writerelevancevectorrecnet.sh ml100k 11 ten
