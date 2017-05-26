81e067392f7a9f32d97cb05add80a8852b865c81
sumit <sumit.sidana@imag.fr>
2017-05-26 14:17:12 +0200

    add: rel vec code

--- writerelevancevectorcofactor.sh
+++ writerelevancevectorcofactor.sh
@@ -4,7 +4,7 @@ mkdir -p /data/sidana/recnet_draft/$1/cofactor/one/rv/
 mkdir -p /data/sidana/recnet_draft/$1/cofactor/one/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/$1/cofactor/vectors/gt_$1 /data/sidana/recnet_draft/$1/cofactor/vectors/pr_$1 /data/sidana/recnet_draft/$1/cofactor/one/rv/relevanceVector_$1 1
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/$1/cofactor/vectors/gt_$1 /data/sidana/recnet_draft/$1/cofactor/vectors/pr_$1 /data/sidana/recnet_draft/$1/cofactor/one/rv/relevanceVector_$1 2
 cd -
 echo 'compute offline metrics'
 python3 compOfflineEvalMetrics_len3.py /data/sidana/recnet_draft/$1/cofactor/one $1
