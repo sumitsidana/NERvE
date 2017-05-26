d9b33071717fdf41d71c422cd80830849dc56926
sumit <sumit.sidana@imag.fr>
2017-05-26 18:20:00 +0200

    add: shellscripts for automation

--- recnet_ml100k.sh
+++ recnet_ml100k.sh
@@ -1,13 +1,13 @@
 mkdir -p /data/sidana/recnet_draft/ml100k/recnet/vectors
 python3 train_test_split.py ml100k 0 1
-/writerelevancevectorrecnet.sh ml100k 01 one
-/writerelevancevectorrecnet.sh ml100k 01 five
-/writerelevancevectorrecnet.sh ml100k 01 ten
+./writerelevancevectorrecnet.sh ml100k 01 one
+./writerelevancevectorrecnet.sh ml100k 01 five
+./writerelevancevectorrecnet.sh ml100k 01 ten
 python3 train_test_split.py ml100k 1 0
-/writerelevancevectorrecnet.sh ml100k 10 one
-/writerelevancevectorrecnet.sh ml100k 10 five
-/writerelevancevectorrecnet.sh ml100k 10 ten
+./writerelevancevectorrecnet.sh ml100k 10 one
+./writerelevancevectorrecnet.sh ml100k 10 five
+./writerelevancevectorrecnet.sh ml100k 10 ten
 python3 train_test_split.py ml100k 1 1
-/writerelevancevectorrecnet.sh ml100k 11 one
-/writerelevancevectorrecnet.sh ml100k 11 five
-/writerelevancevectorrecnet.sh ml100k 11 ten
+./writerelevancevectorrecnet.sh ml100k 11 one
+./writerelevancevectorrecnet.sh ml100k 11 five
+./writerelevancevectorrecnet.sh ml100k 11 ten
