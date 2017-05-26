12bb92c2ab9c46a1dee03c4903f8ea3725886c73
sumit <sumit.sidana@imag.fr>
2017-05-26 17:57:54 +0200

    add: shellscripts for automation

--- writerelevancevectorrecnet.sh
+++ writerelevancevectorrecnet.sh
@@ -1,10 +1,19 @@
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/recnet_draft/$1/recnet/five/rv/
-mkdir -p /data/sidana/recnet_draft/$1/recnet/five/em/
+mkdir -p /data/sidana/recnet_draft/$1/recnet/$2/rv/
+mkdir -p /data/sidana/recnet_draft/$1/recnet/$2/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/$1/recnet/vectors/gt_$1_$2 /data/sidana/recnet_draft/$1/recnet/vectors/pr_$1_$2 /data/sidana/recnet_draft/$1/recnet/five/rv/relevanceVector_$1_$2 5
+if [ $3 == "one" ]
+then
+   rank=2
+elif [ $3 == "five" ]
+then
+   rank=5
+else
+   rank=10
+fi
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/$1/recnet/vectors/gt_$1_$2 /data/sidana/recnet_draft/$1/recnet/vectors/pr_$1_$2 /data/sidana/recnet_draft/$1/recnet/$2/rv/relevanceVector_$1_$2 $3
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len5.py /data/sidana/recnet_draft/$1/recnet/five "$1_$2"
+python3 compOfflineEvalMetrics_len5.py /data/sidana/recnet_draft/$1/recnet/$2 "$1_$2"
