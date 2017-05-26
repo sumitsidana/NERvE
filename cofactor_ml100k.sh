ebb2e0473d87df0c1d1767015cb08716f62836a5
sumit <sumit.sidana@imag.fr>
2017-05-26 14:12:26 +0200

    add: rel vec code

--- cofactor_ml100k.sh
+++ cofactor_ml100k.sh
@@ -6,3 +6,4 @@ echo "running cofactor"
 python Cofactorization_outbrain.py "/data/sidana/recnet_draft/ml100k/cofactor/pro"
 ./writegroundtruthforcofactor.sh ml100k
 ./writepredictorforcofator.sh ml100k
+./writerelevancevectorcofactor.sh ml100k
