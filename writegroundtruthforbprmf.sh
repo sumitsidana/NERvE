fdd6903efcc513e139cb7c41526372da4a8beac4
sumitsidana <sidana.sumit@gmail.com>
2017-05-30 23:41:57 +0200

    typo

--- writegroundtruthforbprmf.sh
+++ writegroundtruthforbprmf.sh
@@ -1,4 +1,4 @@
 javac -cp ~/nnmf_ranking/nnmf_ranking/java/binaries/commons-lang3-3.5.jar  ~/nnmf_ranking/nnmf_ranking/java/preProcess/InputOutput.java ~/nnmf_ranking/nnmf_ranking/java/preProcess/WriteGroundTruthForBPRMF.java
 LANG=en_US.utf8
 mkdir -p  /data/recnet_draft/$1/bprmf/vectors
-java -cp ~/nnmf_ranking/nnmf_ranking/java/ preProcess.WriteGroundTruthForBPRMF  "/data/recnet_draft/$1/bprmf/test.inputbprmf" "/data/recnet_draft/$1/bprmf/vectors/gt_ml20m"
+java -cp ~/nnmf_ranking/nnmf_ranking/java/ preProcess.WriteGroundTruthForBPRMF  "/data/recnet_draft/$1/bprmf/test.inputbprmf" "/data/recnet_draft/$1/bprmf/vectors/gt_$1"
