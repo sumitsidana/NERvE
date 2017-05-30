8637150df5cb43faee4e4a60a82956a9b78b90cb
sumit <sumit.sidana@imag.fr>
2017-05-30 11:39:20 +0200

    add: bprmf

---
+++ java/WriteTrainTestInputBPRMF.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+import preProcess.InputOutput;
+
+public class WriteTrainTestInputBPRMF {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		InputOutput.writeTrainTestInputBPRMF(args[0], args[1], args[2], args[3]);
+	}
+
+}
