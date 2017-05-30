ef551ba941079c720fe742ac0b410927979c4b8b
sumit <sumit.sidana@imag.fr>
2017-05-30 17:14:17 +0200

    add: bprmf

--- bprmf_ml100k.sh
+++ bprmf_ml100k.sh
@@ -2,10 +2,14 @@
 
 java -cp ./java/ preProcess.WriteTrainTestInputBPRMF /data/sidana/recnet_draft/ml100k/bprmf/train_all_raw.csv /data/sidana/recnet_draft/ml100k/bprmf/test_all_raw.csv /data/sidana/recnet_draft/ml100k/bprmf/train.inputbprmf /data/sidana/recnet_draft/ml100k/bprmf/test.inputbprmf
 
+cd /data/sidana/recnet_draft/ml100k/bprmf/
 cut -d$'\t' -f1 test.inputbprmf > test.users
+
+cd /home/ama/sidana/bprmf/mymedialite/bin
 
 sudo ./item_recommendation --training-file=/data/sidana/recnet_draft/ml100k/bprmf/train.inputbprmf --test-file=/data/sidana/recnet_draft/ml100k/bprmf/test.inputbprmf --test-users= --recommender=BPRMF --in-test-items  --prediction-file=/data/sidana/recnet_draft/ml100k/bprmf/prediction_file
 
+cd -
 ./writegroundtruthforbprmf.sh ml100k
 ./writepredictorforbprmf.sh ml100k
 
