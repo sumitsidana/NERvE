ef0b2617820a1c3750ec3c4bc29168d6cb12828b
sumitsidana <sidana.sumit@gmail.com>
2017-06-01 00:35:51 +0200

    add: relevance vector for pi

--- pi_ml100k.sh
+++ pi_ml100k.sh
@@ -1,3 +1,8 @@
 # recnet_draft: user_past_preferences
-sudo java -cp java/ preProcess.GroupInteractionByUserItem /data/user_past_preference.csv /data/userInteractions.csv
+#sudo java -cp java/ preProcess.GroupInteractionByUserItem /data/user_past_preference.csv /data/userInteractions.csv
+#java -cp ./java/ preProcess.TargetUsersPastInteractions /data/sidana/recnet_draft/ml100k/pi/userInteractions.csv /data/sidana/recnet_draft/ml100k/pi/test.users /data/sidana/recnet_draft/ml100k/pi/vectors/pr_ml100k
+
+bash ./writerelevancevectorbprmf.sh ml100k one
+bash ./writerelevancevectorbprmf.sh ml100k five
+bash ./writerelevancevectorbprmf.sh ml100k ten
 
--- writerelevancevectorpi.sh
+++ writerelevancevectorpi.sh
@@ -1,7 +1,7 @@
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/recnet_draft/$1/cofactor/$2/rv/
-mkdir -p /data/sidana/recnet_draft/$1/cofactor/$2/em/
+mkdir -p /data/sidana/recnet_draft/$1/pi/$2/rv/
+mkdir -p /data/sidana/recnet_draft/$1/pi/$2/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
 
@@ -15,7 +15,7 @@ else
    rank=10
 fi
 
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/$1/cofactor/vectors/gt_$1 /data/sidana/recnet_draft/$1/cofactor/vectors/pr_$1 /data/sidana/recnet_draft/$1/cofactor/$2/rv/relevanceVector_$1 $rank
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/$1/pi/vectors/gt_$1 /data/sidana/recnet_draft/$1/pi/vectors/pr_$1 /data/sidana/recnet_draft/$1/pi/$2/rv/relevanceVector_$1 $rank
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet_draft/$1/cofactor/$2 $1
+python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet_draft/$1/pi/$2 $1
