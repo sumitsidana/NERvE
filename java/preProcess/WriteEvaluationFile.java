1af45ece9d8523c9160f14d35454bca4c36b2197
sumit <sumit.sidana@imag.fr>
2017-05-31 11:55:19 +0200

    add: i/o

---
+++ java/preProcess/WriteEvaluationFile.java
@@ -0,0 +1,10 @@
+package preProcess;
+import java.io.*;
+
+public class WriteEvaluationFile {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		InputOutput.writeEvaluationFile(args[0], args[1], args[2]);
+	}
+}
