c75ac0f6bcafe83f6c1e2a6aa20c8f639e02fd8d
sumitsidana <sidana.sumit@gmail.com>
2017-06-01 00:39:43 +0200

    add: relevance vector for pi

--- pi_ml100k.sh
+++ pi_ml100k.sh
@@ -2,7 +2,7 @@
 #sudo java -cp java/ preProcess.GroupInteractionByUserItem /data/user_past_preference.csv /data/userInteractions.csv
 #java -cp ./java/ preProcess.TargetUsersPastInteractions /data/sidana/recnet_draft/ml100k/pi/userInteractions.csv /data/sidana/recnet_draft/ml100k/pi/test.users /data/sidana/recnet_draft/ml100k/pi/vectors/pr_ml100k
 
-bash ./writerelevancevectorbprmf.sh ml100k one
-bash ./writerelevancevectorbprmf.sh ml100k five
-bash ./writerelevancevectorbprmf.sh ml100k ten
+bash ./writerelevancevectorpi.sh ml100k one
+bash ./writerelevancevectorpi.sh ml100k five
+bash ./writerelevancevectorpi.sh ml100k ten
 
