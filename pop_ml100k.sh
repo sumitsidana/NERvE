3a30b7d16c08c9b76c2e390a8d24ab072818dfab
sumit <sumit.sidana@imag.fr>
2017-06-01 10:46:00 +0200

    add pop ml

--- pop_ml100k.sh
+++ pop_ml100k.sh
@@ -1,6 +1,6 @@
 # recnet_draft: Popularity
 # :yy 1,749p
-paste -d' ' test.users mostpopularitems.txt > vectors/pr_ml100k
-bash ./writerelevancevectorpi.sh ml100k one
-bash ./writerelevancevectorpi.sh ml100k five
-bash ./writerelevancevectorpi.sh ml100k ten
+#paste -d' ' test.users mostpopularitems.txt > vectors/pr_ml100k
+bash ./writerelevancevectorpop.sh ml100k one
+bash ./writerelevancevectorpop.sh ml100k five
+bash ./writerelevancevectorpop.sh ml100k ten
