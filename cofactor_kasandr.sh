36010abf4a87055399b31a09c43ce2ac40b14dc7
sumit <sumit.sidana@imag.fr>
2017-06-02 11:17:44 +0200

    add: cofactor for kasandr

--- cofactor_kasandr.sh
+++ cofactor_kasandr.sh
@@ -1,9 +1,9 @@
 cd ~/cofactor/src
 LANG=en_US.utf8
 echo "preprocessing"
-python preprocess_outbrain.py "/data/sidana/recnet_draft/kasandr/cofactor"
+python preprocess.py "/data/sidana/recnet_draft/kasandr/cofactor"
 echo "running cofactor"
-python Cofactorization_outbrain.py "/data/sidana/recnet_draft/kasandr/cofactor/pro"
+python Cofactorization.py "/data/sidana/recnet_draft/kasandr/cofactor/pro"
 ./writegroundtruthforcofactor.sh kasandr
 ./writepredictorforcofator.sh kasandr
 ./writerelevancevectorcofactor.sh kasandr one
