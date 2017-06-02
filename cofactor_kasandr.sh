ff59c2489943decaafde0d4be6de640ecb90eb65
sumit <sumit.sidana@imag.fr>
2017-06-02 11:24:12 +0200

    add: cofactor for kasandr

--- cofactor_kasandr.sh
+++ cofactor_kasandr.sh
@@ -4,6 +4,7 @@ echo "preprocessing"
 python preprocess.py "/data/sidana/recnet_draft/kasandr/cofactor"
 echo "running cofactor"
 python Cofactorization.py "/data/sidana/recnet_draft/kasandr/cofactor/pro"
+cd ~/nnmf_ranking/
 ./writegroundtruthforcofactor.sh kasandr
 ./writepredictorforcofator.sh kasandr
 ./writerelevancevectorcofactor.sh kasandr one
--- cofactor_ml100k.sh
+++ cofactor_ml100k.sh
@@ -4,6 +4,7 @@ echo "preprocessing"
 python preprocess.py "/data/sidana/recnet_draft/ml100k/cofactor"
 echo "running cofactor"
 python Cofactorization.py "/data/sidana/recnet_draft/ml100k/cofactor/pro"
+cd ~/nnmf_ranking/
 ./writegroundtruthforcofactor.sh ml100k
 ./writepredictorforcofator.sh ml100k
 ./writerelevancevectorcofactor.sh ml100k one
