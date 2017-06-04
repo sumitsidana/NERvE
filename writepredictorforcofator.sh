051e116fa83b77b737372ec2aa25de02fca8907e
sumitsidana <sidana.sumit@gmail.com>
2017-06-04 20:06:27 +0200

    add: ground truth

--- writepredictorforcofator.sh
+++ writepredictorforcofator.sh
@@ -1,3 +1,5 @@
 javac -cp ~/embAdaptivity/java/binaries/commons-lang3-3.5.jar  ~/embAdaptivity/java/src/preProcess/InputOutput.java ~/embAdaptivity/java/src/preProcess/WritePredictorForCofactor.java
 LANG=en_US.utf8
-java -cp ~/embAdaptivity/java/src/ preProcess.WritePredictorForCofactor "/data/sidana/recnet_draft/$1/cofactor/pro/test_all.csv" "/data/sidana/recnet_draft/$1/cofactor/pro/pr" "/data/sidana/recnet_draft/$1/cofactor/vectors/pr_$1"
+temp=$1
+var=${temp/\//_}
+java -cp ~/embAdaptivity/java/src/ preProcess.WritePredictorForCofactor "/data/sidana/recnet_draft/$1/cofactor/pro/test_all.csv" "/data/sidana/recnet_draft/$1/cofactor/pro/pr" "/data/sidana/recnet_draft/$1/cofactor/vectors/pr_var"
