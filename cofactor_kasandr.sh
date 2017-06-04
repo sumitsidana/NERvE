aac05c6b7596bde267a22f7fe8217554b9569b2e
sumitsidana <sidana.sumit@gmail.com>
2017-06-04 20:31:19 +0200

    add: ground truth

--- cofactor_kasandr.sh
+++ cofactor_kasandr.sh
@@ -1,12 +1,12 @@
 cd ~/cofactor/src
 LANG=en_US.utf8
 echo "preprocessing"
-python preprocess.py "/data/sidana/recnet_draft/kasandr/atleast_five_o_two_c/cofactor"
+python preprocess.py "/data/sidana/recnet_draft/kasandr/atleast_ten_o_two_c/cofactor"
 echo "running cofactor"
-python Cofactorization.py "/data/sidana/recnet_draft/kasandr/atleast_five_o_two_c/cofactor/pro"
+python Cofactorization.py "/data/sidana/recnet_draft/kasandr/atleast_ten_o_two_c/cofactor/pro"
 cd ~/nnmf_ranking/
-./writegroundtruthforcofactor.sh kasandr/atleast_five_o_two_c
-./writepredictorforcofator.sh kasandr/atleast_five_o_two_c
-./writerelevancevectorcofactor.sh kasandr/atleast_five_o_two_c one
-./writerelevancevectorcofactor.sh kasandr/atleast_five_o_two_c five
-./writerelevancevectorcofactor.sh kasandr/atleast_five_o_two_c ten
+./writegroundtruthforcofactor.sh kasandr/atleast_ten_o_two_c
+./writepredictorforcofator.sh kasandr/atleast_ten_o_two_c
+./writerelevancevectorcofactor.sh kasandr/atleast_ten_o_two_c one
+./writerelevancevectorcofactor.sh kasandr/atleast_ten_o_two_c ten
+./writerelevancevectorcofactor.sh kasandr/atleast_ten_o_two_c ten
