a928c3f97c80372e3392ea022b21e9d137e2ef4d
sumitsidana <sidana.sumit@gmail.com>
2017-06-18 17:46:33 +0200

    add: cofactor ml1m

--- cofactor_ml1m.sh
+++ cofactor_ml1m.sh
@@ -1,4 +1,5 @@
 mkdir -p /data/sidana/recnet_draft/ml1m/cofactor/pro
+cp /data/sidana/recnet_draft/ml1m/ratings.csv /data/sidana/recnet_draft/ml1m/cofactor/
 cd ~/cofactor/src
 LANG=en_US.utf8
 echo "preprocessing"
