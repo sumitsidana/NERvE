b4de2f21e057dbfcfd7444cbc5e7c0481452bd3d
sumitsidana <sidana.sumit@gmail.com>
2017-06-18 17:50:32 +0200

    add recnet ml1m

---
+++ recnet_ml1m.sh
@@ -0,0 +1,14 @@
+mkdir -p /data/sidana/recnet_draft/ml1m/recnet/vectors
+cp /data/sidana/recnet_draft/ml1m/ratings.csv /data/sidana/recnet_draft/ml1m/recnet/
+python3 train_test_split.py ml1m 0 1
+./writerelevancevectorrecnet.sh ml1m 01 one
+./writerelevancevectorrecnet.sh ml1m 01 five
+./writerelevancevectorrecnet.sh ml1m 01 ten
+python3 train_test_split.py ml1m 1 0
+./writerelevancevectorrecnet.sh ml1m 10 one
+./writerelevancevectorrecnet.sh ml1m 10 five
+./writerelevancevectorrecnet.sh ml1m 10 ten
+python3 train_test_split.py ml1m 1 1
+./writerelevancevectorrecnet.sh ml1m 11 one
+./writerelevancevectorrecnet.sh ml1m 11 five
+./writerelevancevectorrecnet.sh ml1m 11 ten
