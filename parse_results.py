c78ede4b77c5454d88cdfaccbccd3dd3512a1858
Charlotte Laclau <laclauc@tiger.imag.fr>
2017-06-27 11:21:47 +0200

    bestcodeever

--- parse_results.py
+++ parse_results.py
@@ -3,16 +3,17 @@ with open('results') as infile, open('results_output', 'w') as outfile:
         if not line.strip(): continue  # skip the empty line
         outfile.write(line)  # non-empty line. Write it to output
 	
-f=open('results_output', 'r')
-f1=open('model_0_1', 'w')
+f = open('results_output', 'r')
+f1 = open('model_0_1', 'w')
 f2 = open('model_1_0', 'w')
 f3 = open('model_1_1', 'w')
-flag_0_1=False
-flag_1_0=False
-flag_1_1=False
+
+flag_0_1 = False
+flag_1_0 = False
+flag_1_1 = False
 
 for line in f:
-    #print(line)
+
     line = line.strip('\n')
 
     if line == "alpha: 0, beta: 1":
@@ -36,21 +37,54 @@ for line in f:
         f2.write('\n')
         continue
 
-    if flag_0_1 == True:
-        f1.write(line[7:]+" ")
-        continue
+    if flag_0_1:
+        if "map@10" in line:
+            f1.write(line[7:])
+        else:
+            f1.write(line[7:]+" ")
+            continue
 
-    if flag_1_0 == True:
-        f2.write(line[7:]+ " ")
-        continue
+    if flag_1_0:
+        if "map@10" in line:
+            f2.write(line[7:])
+        else:
+            f2.write(line[7:]+" ")
+            continue
 
-    if flag_1_1 == True:
-        f3.write(line[7:] + " ")
-        continue
+    if flag_1_1:
+        if "map@10" in line:
+            f3.write(line[7:])
+        else:
+            f3.write(line[7:]+" ")
+            continue
+
 
 f1.close()
 f2.close()
 f3.close()
-f.close
+f.close()
 
 
+#f1 = open('model_0_1', "r+")
+
+#Move the pointer (similar to a cursor in a text editor) to the end of the file.
+#f1.seek(0, SEEK_END)
+
+#This code means the following code skips the very last character in the file -
+#i.e. in the case the last line is null we delete the last line
+#and the penultimate one
+#pos = f1.tell() - 1
+
+#Read each character in the file one at a time from the penultimate
+#character going backwards, searching for a newline character
+#If we find a new line, exit the search
+#while pos > 0 and f1.read(1) != "\n":
+#    pos -= 1
+#    f1.seek(pos, SEEK_SET)
+
+#So long as we're not at the start of the file, delete all the characters ahead of this position
+#if pos > 0:
+#    f1.seek(pos, SEEK_SET)
+#    f1.truncate()
+
+#f1.close()
