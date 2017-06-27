ccca9df5cfa7b21cf1b31e2fa8ad744ffd6db0ae
sumit <sumit.sidana@imag.fr>
2017-06-27 21:14:13 +0200

    parameter tuning for ml1m and ml100k

--- recnet_ml1m_param_tune.sh
+++ recnet_ml1m_param_tune.sh
@@ -5,7 +5,7 @@ end_lf=20
 reg_params="0.0001 0.001 0.005 0.01 0.05"
 hidden_units="16 32 64"
 
-for  latent_factor in $(seq 1 $END); do
+for  latent_factor in $(seq 1 $end_lf); do
     for reg in $reg_params; do
         for num_units in $hidden_units; do
             echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet_draft/param_tune/ml1m/recnet/results
