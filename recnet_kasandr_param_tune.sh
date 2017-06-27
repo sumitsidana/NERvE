2a45590056dd5e632ccf67c0d45b43dfc2103871
sumit <sumit.sidana@imag.fr>
2017-06-27 17:00:16 +0200

    add: recnet kasandr

--- recnet_kasandr_param_tune.sh
+++ recnet_kasandr_param_tune.sh
@@ -6,8 +6,8 @@ reg_params="0.0001 0.001 0.005 0.01 0.05"
 hidden_units="16 32 64"
 
 for  latent_factor in $(seq 1 $END); do
-    for reg in reg_params; do
-        for num_units in hidden_units; do
+    for reg in $reg_params; do
+        for num_units in $hidden_units; do
             echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             python3 recnet_param_tune.py kasandr 0 1 $latent_factor $reg $num_units
             echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
