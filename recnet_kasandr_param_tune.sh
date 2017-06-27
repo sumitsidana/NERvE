2fb031b416282d8b305161779fb9c24fd5d292b5
sumit <sumit.sidana@imag.fr>
2017-06-27 16:57:47 +0200

    remove  new line

--- recnet_kasandr_param_tune.sh
+++ recnet_kasandr_param_tune.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 mkdir -p /data/sidana/recnet_draft/param_tune/kasandr/recnet/vectors
+
 end_lf=20
 reg_params="0.0001 0.001 0.005 0.01 0.05"
 hidden_units="16 32 64"
+
 for  latent_factor in $(seq 1 $END); do
     for reg in reg_params; do
         for num_units in hidden_units; do
@@ -18,7 +20,7 @@ for  latent_factor in $(seq 1 $END); do
 
             python3 recnet_param_tune.py kasandr 1 0 $latent_factor $reg $num_units
 
-            echo -e "alpha: 1, beta: 0\n" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e "alpha: 1, beta: 0" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             echo -e -n "map@1:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 10 one>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             echo -e -n "map@5:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
