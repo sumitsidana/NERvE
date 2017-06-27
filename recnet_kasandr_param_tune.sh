8c4eec280f2642548e079bcbb7f857b865437fcf
sumit <sumit.sidana@imag.fr>
2017-06-27 17:54:11 +0200

    add: recnet kasandr

--- recnet_kasandr_param_tune.sh
+++ recnet_kasandr_param_tune.sh
@@ -11,29 +11,29 @@ for  latent_factor in $(seq 1 $END); do
             echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             python3 recnet_param_tune.py kasandr 0 1 $latent_factor $reg $num_units
             echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-            echo -e -n "map@1:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 01 one >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-            echo -e -n "map@5:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 01 five>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-            echo -e -n "map@10: " >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 01 ten >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
 
             python3 recnet_param_tune.py kasandr 1 0 $latent_factor $reg $num_units
 
             echo -e "alpha: 1, beta: 0" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-            echo -e -n "map@1:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 10 one>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-            echo -e -n "map@5:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 10 five>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-            echo -e -n "map@10: " >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 10 ten>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
 
             python3 recnet_param_tune.py kasandr 1 1 $latent_factor $reg $num_units
 
            echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-           echo -e -n "map@1:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+           echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 11 one>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
-           echo -e -n "map@5:">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
+           echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 11 five>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
            echo -e -n "map@10: " >>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
             ./writerelevancevectorrecnet_param_tune.sh kasandr 11 ten>>/data/sidana/recnet_draft/param_tune/kasandr/recnet/results
