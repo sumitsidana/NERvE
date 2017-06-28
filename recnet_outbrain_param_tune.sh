eda1df1b6a9de21ae2dd9d79854fd6732795105f
sumit <sumit.sidana@imag.fr>
2017-06-28 14:34:41 +0200

    add: parameter tuning for recnet

--- recnet_outbrain_param_tune.sh
+++ recnet_outbrain_param_tune.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p /data/sidana/recnet_draft/param_tune/ml100k/recnet/vectors
+mkdir -p /data/sidana/recnet_draft/param_tune/outbrain/recnet/vectors
 
 end_lf=20
 reg_params="0.0001 0.001 0.005 0.01 0.05"
@@ -9,37 +9,37 @@ for  latent_factor in $(seq 1 $end_lf); do
     for reg in $reg_params; do
         for num_units in $hidden_units; do
 
-            echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+            echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet_draft/param_tune/outbrain/recnet/results
 
-            python3 recnet_param_tune.py ml100k 0 1 $latent_factor $reg $num_units
+            python3 recnet_param_tune.py outbrain 0 1 $latent_factor $reg $num_units
 
-            echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 one >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 five>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 ten >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+            echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 01 one >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 01 five>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 01 ten >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
 
-            python3 recnet_param_tune.py ml100k 1 0 $latent_factor $reg $num_units
+            python3 recnet_param_tune.py outbrain 1 0 $latent_factor $reg $num_units
 
-            echo -e "alpha: 1, beta: 0\n" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 one>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 five>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 ten>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+            echo -e "alpha: 1, beta: 0\n" >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 10 one>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 10 five>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 10 ten>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
 
-            python3 recnet_param_tune.py ml100k 1 1 $latent_factor $reg $num_units
+            python3 recnet_param_tune.py outbrain 1 1 $latent_factor $reg $num_units
 
-           echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-           echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 one>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-           echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 five>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-           echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 ten>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+           echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+           echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 11 one>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+           echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 11 five>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+           echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh outbrain 11 ten>>/data/sidana/recnet_draft/param_tune/outbrain/recnet/results
         done
     done
 done
