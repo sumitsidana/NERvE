493d9b316e7c3dbe1e3c054ce7b4e07f4568b1c7
sumitsidana <sidana.sumit@gmail.com>
2017-07-11 01:09:15 +0200

    add: bprmf

--- bprmf_ml100k.sh
+++ bprmf_ml100k.sh
@@ -5,7 +5,7 @@ java -cp ./java/ preProcess.WriteTrainTestInputBPRMF /data/recnet_draft/ml100k/b
 cd /data/recnet_draft/ml100k/bprmf/
 cut -f1 test.inputbprmf > test.users
 cd /home/sumit/mymedialite/bin/
-./item_recommendation --training-file=/data/recnet_draft/ml100k/bprmf/train.inputbprmf --test-file=/data/recnet_draft/ml100k/bprmf/test.inputbprmf --test-users=/data/recnet_draft/ml100k/bprmf/test.users --recommender=BPRMF --recommender-options="num_factors=50 reg_u=0.0001 --reg_i=0.0001 --reg_j=0.0001 --learn_rate=0.001 --num_factors=3" --in-test-items  --prediction-file=/data/recnet_draft/ml100k/bprmf/prediction_file
+./item_recommendation --training-file=/data/recnet_draft/ml100k/bprmf/train.inputbprmf --test-file=/data/recnet_draft/ml100k/bprmf/test.inputbprmf --test-users=/data/recnet_draft/ml100k/bprmf/test.users --recommender=BPRMF --recommender-options=num_factors=50 reg_u=0.0001 reg_i=0.0001 reg_j=0.0001 learn_rate=0.001 num_factors=3 --in-test-items  --prediction-file=/data/recnet_draft/ml100k/bprmf/prediction_file
 
 cd /home/sumit/nnmf_ranking/nnmf_ranking
 ./writegroundtruthforbprmf.sh ml100k
