4b30f277d0f52c6ba6e4eda4baebdb093b22b145
sumit <sumit.sidana@imag.fr>
2017-07-19 10:42:14 +0200

    add: bprmf_kasandr

--- bprmf_kasandr.sh
+++ bprmf_kasandr.sh
@@ -5,7 +5,7 @@ java -cp ./java/ preProcess.WriteTrainTestInputBPRMF /data/recnet_draft/kasandr/
 cd /data/recnet_draft/kasandr/bprmf/
 cut -f1 test.inputbprmf > test.users
 cd /home/sumit/mymedialite/bin/
-./item_recommendation --training-file=/data/recnet_draft/kasandr/bprmf/train.inputbprmf --test-file=/data/recnet_draft/kasandr/bprmf/test.inputbprmf --test-users=/data/recnet_draft/kasandr/bprmf/test.users --recommender=BPRMF --in-test-items  --prediction-file=/data/recnet_draft/kasandr/bprmf/prediction_file
+./item_recommendation --training-file=/data/recnet_draft/kasandr/bprmf/train.inputbprmf --test-file=/data/recnet_draft/kasandr/bprmf/test.inputbprmf --test-users=/data/recnet_draft/kasandr/bprmf/test.users --recommender=BPRMF --recommender-options=reg_u=0.0001 --recommender-options=reg_i=0.0001 --recommender-options=reg_j=0.0001 --recommender-options=learn_rate=0.001 --recommender-options=num_factors=3 --in-test-items  --prediction-file=/data/recnet_draft/kasandr/bprmf/prediction_file
 
 cd /home/sumit/recnet_draft
 ./writegroundtruthforbprmf.sh kasandr
