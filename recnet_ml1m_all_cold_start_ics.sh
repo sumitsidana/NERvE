b6c62260a50382632a75f8295b8aa03b786bae23
sumitsidana <sidana.sumit@gmail.com>
2017-07-23 11:53:46 +0200

    add: ml1m cold start recnet

--- recnet_ml1m_all_cold_start_ics.sh
+++ recnet_ml1m_all_cold_start_ics.sh
@@ -1,13 +1,13 @@
-mkdir -p /data/sidana/recnet_draft/cold_start/ucs/ml1m/recnet_all/vectors
-python3 train_test_split_all_cold_start_ucs.py ml1m 0 1
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 01 one
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 01 five
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 01 ten
-python3 train_test_split_all_cold_start_ucs.py ml1m 1 0
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 10 one
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 10 five
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 10 ten
-python3 train_test_split_all_cold_start_ucs.py ml1m 1 1
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 11 one
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 11 five
-./writerelevancevectorrecnet_all_cold_start_ucs.sh ml1m 11 ten
+mkdir -p /data/sidana/recnet_draft/cold_start/ics/ml1m/recnet_all/vectors
+python3 train_test_split_all_cold_start_ics.py ml1m 0 1
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 01 one
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 01 five
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 01 ten
+python3 train_test_split_all_cold_start_ics.py ml1m 1 0
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 10 one
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 10 five
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 10 ten
+python3 train_test_split_all_cold_start_ics.py ml1m 1 1
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 11 one
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 11 five
+./writerelevancevectorrecnet_all_cold_start_ics.sh ml1m 11 ten
--- writerelevancevectorrecnet_all_cold_start_ics.sh
+++ writerelevancevectorrecnet_all_cold_start_ics.sh
@@ -1,7 +1,7 @@
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3/rv/
-mkdir -p /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3/em/
+mkdir -p /data/sidana/recnet_draft/cold_start/ics/$1/recnet_all/$3/rv/
+mkdir -p /data/sidana/recnet_draft/cold_start/ics/$1/recnet_all/$3/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
 if [ $3 == "one" ]
@@ -13,7 +13,7 @@ then
 else
    rank=10
 fi
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/vectors/gt_$1_$2 /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/vectors/pr_$1_$2 /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3/rv/relevanceVector_$1_$2 $rank
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/cold_start/ics/$1/recnet_all/vectors/gt_$1_$2 /data/sidana/recnet_draft/cold_start/ics/$1/recnet_all/vectors/pr_$1_$2 /data/sidana/recnet_draft/cold_start/ics/$1/recnet_all/$3/rv/relevanceVector_$1_$2 $rank
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3 "$1_$2"
+python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet_draft/cold_start/ics/$1/recnet_all/$3 "$1_$2"
