d182447650ae8cd42c231ed94b2cb931d65ab9e7
sumitsidana <sidana.sumit@gmail.com>
2017-07-23 11:40:29 +0200

    add: ml1m cold start recnet

---
+++ writerelevancevectorrecnet_all_cold_start_ucs.sh
@@ -0,0 +1,19 @@
+LANG=en_US.utf8
+cd java/
+mkdir -p /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3/rv/
+mkdir -p /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3/em/
+javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
+echo 'making relevance vector'
+if [ $3 == "one" ]
+then
+   rank=2
+elif [ $3 == "five" ]
+then
+   rank=5
+else
+   rank=10
+fi
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/vectors/gt_$1_$2 /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/vectors/pr_$1_$2 /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3/rv/relevanceVector_$1_$2 $rank
+cd -
+echo 'compute offline metrics'
+python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet_draft/cold_start/ucs/$1/recnet_all/$3 "$1_$2"
