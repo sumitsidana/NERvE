5be478bed0eff85f496e62d5bf866e3038ebf0d2
sumit <sumit.sidana@imag.fr>
2017-07-24 10:20:43 +0200

    add cofactor for netflix

--- cofactor_netflix.sh
+++ cofactor_netflix.sh
@@ -1,12 +1,12 @@
 cd ~/cofactor/src
 LANG=en_US.utf8
 echo "preprocessing"
-python preprocess.py "/data/sidana/recnet_draft/cofactor/cofactor"
+python preprocess.py "/data/sidana/recnet_draft/netflix/cofactor"
 echo "running cofactor"
-python Cofactorization.py "/data/sidana/recnet_draft/cofactor/cofactor/pro"
+python Cofactorization.py "/data/sidana/recnet_draft/netflix/cofactor/pro"
 cd ~/nnmf_ranking/
-./writegroundtruthforcofactor.sh cofactor
-./writepredictorforcofator.sh cofactor
-./writerelevancevectorcofactor.sh cofactor one
-./writerelevancevectorcofactor.sh cofactor five
-./writerelevancevectorcofactor.sh cofactor ten
+./writegroundtruthforcofactor.sh netflix
+./writepredictorforcofator.sh netflix
+./writerelevancevectorcofactor.sh netflix one
+./writerelevancevectorcofactor.sh netflix five
+./writerelevancevectorcofactor.sh netflix ten
