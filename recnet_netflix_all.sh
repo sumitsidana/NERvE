f75f394bacb4ee30d6a13ec6be20747cafed3589
sumit <sumit.sidana@imag.fr>
2017-07-24 11:53:57 +0200

    recnet netflix all

--- recnet_netflix_all.sh
+++ recnet_netflix_all.sh
@@ -1,13 +1,13 @@
-mkdir -p /data/sidana/recnet_draft/kasandr/recnet_all/vectors
-python3 train_test_split_all.py kasandr 0 1
-./writerelevancevectorrecnet_all.sh kasandr 01 one
-./writerelevancevectorrecnet_all.sh kasandr 01 five
-./writerelevancevectorrecnet_all.sh kasandr 01 ten
-python3 train_test_split_all.py kasandr 1 0
-./writerelevancevectorrecnet_all.sh kasandr 10 one
-./writerelevancevectorrecnet_all.sh kasandr 10 five
-./writerelevancevectorrecnet_all.sh kasandr 10 ten
-python3 train_test_split_all.py kasandr 1 1
-./writerelevancevectorrecnet_all.sh kasandr 11 one
-./writerelevancevectorrecnet_all.sh kasandr 11 five
-./writerelevancevectorrecnet_all.sh kasandr 11 ten
+mkdir -p /data/sidana/recnet_draft/netflix/recnet_all/vectors
+python3 train_test_split_all.py netflix 0 1
+./writerelevancevectorrecnet_all.sh netflix 01 one
+./writerelevancevectorrecnet_all.sh netflix 01 five
+./writerelevancevectorrecnet_all.sh netflix 01 ten
+python3 train_test_split_all.py netflix 1 0
+./writerelevancevectorrecnet_all.sh netflix 10 one
+./writerelevancevectorrecnet_all.sh netflix 10 five
+./writerelevancevectorrecnet_all.sh netflix 10 ten
+python3 train_test_split_all.py netflix 1 1
+./writerelevancevectorrecnet_all.sh netflix 11 one
+./writerelevancevectorrecnet_all.sh netflix 11 five
+./writerelevancevectorrecnet_all.sh netflix 11 ten
