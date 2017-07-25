eda055e0e446fdd73b68858bd5e2d19ff8da5f62
sumit <sumit.sidana@imag.fr>
2017-07-25 11:12:06 +0200

    add: light fm ml100k user cold start

---
+++ lightfm_ml100k_ucs.sh
@@ -0,0 +1,13 @@
+mkdir -p /data/sidana/recnet_draft/cold_start/ucs/ml100k/lightfm_all/vectors/
+sed -i '1d' /data/sidana/recnet_draft/cold_start/ucs/ml100k/lightfm_all/train_all_raw.csv
+sed -i '1d' /data/sidana/recnet_draft/cold_start/ucs/ml100k/lightfm_all/test_all_raw.csv
+python3 lightfm_all_module_ucs.py ml100k ","
+directoryofvectors='/data/sidana/recnet_draft/cold_start/ucs/ml100k/lightfm_all/vectors/'
+cd $directoryofvectors
+echo 'removing duplicates'
+cat gt | uniq > gt_ml1m
+cat pr | uniq > pr_ml1m
+cd -
+./writerelevancevectorlightfm_all_ucs.sh ml100k one
+./writerelevancevectorlightfm_all_ucs.sh ml100k five
+./writerelevancevectorlightfm_all_ucs.sh ml100k ten
