b73227bec6949c1fb92f0e97b8361c2305da5721
sumit <sumit.sidana@imag.fr>
2017-09-07 14:14:16 +0200

    parsh alpha files

--- parse_results_alpha.py
+++ parse_results_alpha.py
@@ -6,13 +6,12 @@
 # f = open('results_output', 'r')
 import sys
 f = open('/data/sidana/recnet_draft/'+sys.argv[1]+'/recnet_alpha/results', 'r')
-f1 = open('/data/sidana/recnet_draft/'+sys.argv[1]+'/recnet_alpha/batches_'+sys.argv[1], 'w')
+f1 = open('/data/sidana/recnet_draft/'+sys.argv[1]+'/recnet_alpha/alpha_'+sys.argv[1], 'w')
 
 flag_0_1 = False
 flag_1_0 = False
 flag_1_1 = False
 
-flag_write=False
 
 for line in f:
 
@@ -39,16 +38,14 @@ for line in f:
     if "alpha_value:" in line:
         index = line.index(" ",13)
         num_batches = line[13:index]
-        flag_write = True
         f1.write(num_batches + " ")
     else:
         map1 = line[7:]
-        if flag_write:
-            if flag_1_1:
-                f1.write(map1+"\n")
-                flag_write = False
-            else:
-                f1.write(map1+" ")
+        if flag_1_1:
+            f1.write(map1+"\n")
+            flag_write = False
+        else:
+            f1.write(map1+" ")
 
 f1.close()
 f.close()
