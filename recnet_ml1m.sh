cf6d79d95681685e06cb9d9d140a285c12166dac
sumit <sumit.sidana@imag.fr>
2018-02-06 11:41:24 +0100

    add proper reg param

--- recnet_ml1m.sh
+++ recnet_ml1m.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 mkdir -p /data/sidana/recnet/ml1m/recnet/vectors
-python3 train_test_split.py ml1m 0 1 2 64
+python3 train_test_split.py ml1m 0 1 1 32 0.0001
 
 ./writerelevancevectorrecnet.sh ml1m 01 one
 ./writerelevancevectorrecnet.sh ml1m 01 five
 ./writerelevancevectorrecnet.sh ml1m 01 ten
 
-python3 train_test_split.py ml1m 1 0 14 32
+python3 train_test_split.py ml1m 1 0 8 64 0.005
 
 ./writerelevancevectorrecnet.sh ml1m 10 one
 ./writerelevancevectorrecnet.sh ml1m 10 five
 ./writerelevancevectorrecnet.sh ml1m 10 ten
 
-python3 train_test_split.py ml1m 1 1 3 64
+python3 train_test_split.py ml1m 1 1 1 32 0.005
 
 ./writerelevancevectorrecnet.sh ml1m 11 one
 ./writerelevancevectorrecnet.sh ml1m 11 five
