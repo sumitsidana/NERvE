638470155a561d6b15171f95472d88e60b182f81
sumit <sumit.sidana@imag.fr>
2018-02-06 12:01:51 +0100

    prepare to re-run recnet on netflix

--- recnet_netflix.sh
+++ recnet_netflix.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-mkdir -p /data/sidana/recnet/ml100k/recnet/vectors
-python3 train_test_split.py ml100k 0 1 2 64 0.005
+mkdir -p /data/sidana/recnet/netflix/recnet/vectors
+python3 train_test_split.py netflix 0 1 2 16 0.05
 
-./writerelevancevectorrecnet.sh ml100k 01 one
-./writerelevancevectorrecnet.sh ml100k 01 five
-./writerelevancevectorrecnet.sh ml100k 01 ten
+./writerelevancevectorrecnet.sh netflix 01 one
+./writerelevancevectorrecnet.sh netflix 01 five
+./writerelevancevectorrecnet.sh netflix 01 ten
 
-python3 train_test_split.py ml100k 1 0 14 32 0.01
+python3 train_test_split.py netflix 1 0 14 64 0.05
 
-./writerelevancevectorrecnet.sh ml100k 10 one
-./writerelevancevectorrecnet.sh ml100k 10 five
-./writerelevancevectorrecnet.sh ml100k 10 ten
+./writerelevancevectorrecnet.sh netflix 10 one
+./writerelevancevectorrecnet.sh netflix 10 five
+./writerelevancevectorrecnet.sh netflix 10 ten
 
-python3 train_test_split.py ml100k 1 1 3 64 0.0001
+python3 train_test_split.py netflix 1 1 4 32 0.05
 
-./writerelevancevectorrecnet.sh ml100k 11 one
-./writerelevancevectorrecnet.sh ml100k 11 five
-./writerelevancevectorrecnet.sh ml100k 11 ten
+./writerelevancevectorrecnet.sh netflix 11 one
+./writerelevancevectorrecnet.sh netflix 11 five
+./writerelevancevectorrecnet.sh netflix 11 ten
