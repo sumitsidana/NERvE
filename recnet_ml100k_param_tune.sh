4c55151ef558315f015d63fc18b698ae9f610bbb
sumit <sumit.sidana@imag.fr>
2018-02-06 18:17:03 +0100

    parameter tuning scripts

--- recnet_ml100k_param_tune.sh
+++ recnet_ml100k_param_tune.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p /data/sidana/recnet_draft/param_tune/ml100k/recnet/vectors
+mkdir -p /data/sidana/recnet/param_tune/ml100k/recnet/vectors
 
 end_lf=20
 reg_params="0.0001 0.001 0.005 0.01 0.05"
@@ -9,37 +9,37 @@ for  latent_factor in $(seq 1 $end_lf); do
     for reg in $reg_params; do
         for num_units in $hidden_units; do
 
-            echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+            echo -e "Latent Factor: $latent_factor Regularization: $reg Hidden Units: $num_units" >> /data/sidana/recnet/param_tune/ml100k/recnet/results
 
             python3 recnet_param_tune.py ml100k 0 1 $latent_factor $reg $num_units
 
-            echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 one >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 five>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 ten >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+            echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            echo -e -n "map@1: ">>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 one >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            echo -e -n "map@5: ">>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 five>>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            echo -e -n "map@10:" >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 01 ten >>/data/sidana/recnet/param_tune/ml100k/recnet/results
 
             python3 recnet_param_tune.py ml100k 1 0 $latent_factor $reg $num_units
 
-            echo -e "alpha: 1, beta: 0" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 one>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 five>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 ten>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+            echo -e "alpha: 1, beta: 0" >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            echo -e -n "map@1: ">>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 one>>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            echo -e -n "map@5: ">>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 five>>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            echo -e -n "map@10:" >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 10 ten>>/data/sidana/recnet/param_tune/ml100k/recnet/results
 
             python3 recnet_param_tune.py ml100k 1 1 $latent_factor $reg $num_units
 
-           echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-           echo -e -n "map@1: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 one>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-           echo -e -n "map@5: ">>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 five>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-           echo -e -n "map@10:" >>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
-            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 ten>>/data/sidana/recnet_draft/param_tune/ml100k/recnet/results
+           echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+           echo -e -n "map@1: ">>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 one>>/data/sidana/recnet/param_tune/ml100k/recnet/results
+           echo -e -n "map@5: ">>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 five>>/data/sidana/recnet/param_tune/ml100k/recnet/results
+           echo -e -n "map@10:" >>/data/sidana/recnet/param_tune/ml100k/recnet/results
+            ./writerelevancevectorrecnet_param_tune.sh ml100k 11 ten>>/data/sidana/recnet/param_tune/ml100k/recnet/results
         done
     done
 done
--- writerelevancevectorrecnet_param_tune.sh
+++ writerelevancevectorrecnet_param_tune.sh
@@ -1,6 +1,6 @@
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/recnet_draft/param_tune/$1/recnet/$3/rv/
+mkdir -p /data/sidana/recnet/param_tune/$1/recnet/$3/rv/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 if [ $3 == "one" ]
 then
@@ -11,6 +11,6 @@ then
 else
    rank=10
 fi
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/param_tune/$1/recnet/vectors/gt_$1_$2 /data/sidana/recnet_draft/param_tune/$1/recnet/vectors/pr_$1_$2 /data/sidana/recnet_draft/param_tune/$1/recnet/$3/rv/relevanceVector_$1_$2 $rank
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet/param_tune/$1/recnet/vectors/gt_$1_$2 /data/sidana/recnet/param_tune/$1/recnet/vectors/pr_$1_$2 /data/sidana/recnet/param_tune/$1/recnet/$3/rv/relevanceVector_$1_$2 $rank
 cd - >> uselessfile
-python3 compOfflineEvalMetrics_len${rank}_param_tune.py /data/sidana/recnet_draft/param_tune/$1/recnet/$3 "$1_$2"
+python3 compOfflineEvalMetrics_len${rank}_param_tune.py /data/sidana/recnet/param_tune/$1/recnet/$3 "$1_$2"
