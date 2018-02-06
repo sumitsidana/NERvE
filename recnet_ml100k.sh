aec32744f6819bea8754cc8034b3e04e1ae81821
sumit <sumit.sidana@imag.fr>
2018-02-06 10:51:19 +0100

    correct paths

--- recnet_ml100k.sh
+++ recnet_ml100k.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 mkdir -p /data/sidana/recnet/ml100k/recnet/vectors
-python3 train_test_split.py ml100k 0 1 14 32
+python3 train_test_split.py ml100k 0 1 2 64
 
 ./writerelevancevectorrecnet.sh ml100k 01 one
 ./writerelevancevectorrecnet.sh ml100k 01 five
 ./writerelevancevectorrecnet.sh ml100k 01 ten
 
-python3 train_test_split.py ml100k 1 0 2 64
+python3 train_test_split.py ml100k 1 0 14 32
 
 ./writerelevancevectorrecnet.sh ml100k 10 one
 ./writerelevancevectorrecnet.sh ml100k 10 five
