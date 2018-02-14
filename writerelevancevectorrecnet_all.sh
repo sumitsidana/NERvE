ea2713a8505cfcfc39cca4686cfcbe7e0ffe3096
sumit <sumit.sidana@imag.fr>
2018-02-14 11:24:54 +0100

    recnet kasandr

--- writerelevancevectorrecnet_all.sh
+++ writerelevancevectorrecnet_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/recnet/$1/recnet_all/$3/rv/
-mkdir -p /data/sidana/recnet/$1/recnet_all/$3/em/
+mkdir -p /data/sidana/recnet/all_test_param_tune/$1/recnet_all/$3/rv/
+mkdir -p /data/sidana/recnet/all_test_param_tune/$1/recnet_all/$3/em/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 echo 'making relevance vector'
 if [ $3 == "one" ]
@@ -14,7 +14,7 @@ then
 else
    rank=10
 fi
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet/$1/recnet_all/vectors/gt_$1_$2 /data/sidana/recnet/$1/recnet_all/vectors/pr_$1_$2 /data/sidana/recnet/$1/recnet_all/$3/rv/relevanceVector_$1_$2 $rank
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet/all_test_param_tune/$1/recnet_all/vectors/gt_$1_$2 /data/sidana/recnet/all_test_param_tune/$1/recnet_all/vectors/pr_$1_$2 /data/sidana/recnet/all_test_param_tune/$1/recnet_all/$3/rv/relevanceVector_$1_$2 $rank
 cd -
 echo 'compute offline metrics'
-python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet/$1/recnet_all/$3 "$1_$2"
+python3 compOfflineEvalMetrics_len$rank.py /data/sidana/recnet/all_test_param_tune/$1/recnet_all/$3 "$1_$2"
