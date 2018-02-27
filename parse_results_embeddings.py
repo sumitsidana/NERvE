16a13bcee63e394a2b044df4d96180d50596bbe1
sumit <sumit.sidana@imag.fr>
2018-02-27 11:24:03 +0100

    turn flag false on all occasions

--- parse_results_embeddings.py
+++ parse_results_embeddings.py
@@ -8,13 +8,15 @@ import sys
 f = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/results', 'r')
 f1 = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/embeddings_'+sys.argv[1], 'w')
 
+flag_latent_factor = False
+
 flag_0_1 = False
 flag_1_0 = False
 flag_1_1 = False
 
-flag_0_1_write=False
-flag_1_0_write = False
-flag_1_1_write = False
+flag_0_1_param_write=False
+flag_1_0_param_write = False
+flag_1_1_param_write = False
 
 for line in f:
 
@@ -41,39 +43,41 @@ for line in f:
     if "Latent Factor" in line:
         index = line.index(" ",15)
         latent_factor = line[15:index]
+        if flag_latent_factor:
+            f1.write(latent_factor + " ")
+            flag_latent_factor = False
+            # need to turn it into True after writing results of 1 1
 
 
-    if "Latent Factor" in line and flag_0_1:
-        f1.write(latent_factor + " ")
-
-    if "Latent Factor" in line and flag_1_0:
-        f1.write(latent_factor + " ")
-
-    if "Latent Factor" in line and flag_1_1:
-        f1.write(latent_factor + " ")
 
     if "Regularization: 0.005 Hidden Units: 64" in line:
-        flag_0_1_write = True
+        flag_0_1_param_write = True
 
     if "Regularization: 0.05 Hidden Units: 32" in line:
-        flag_1_0_write = True
+        flag_1_0_param_write = True
 
     if "Regularization: 0.005 Hidden Units: 16" in line:
-        flag_1_1_write = True
+        flag_1_1_param_write = True
 
 
 
     if "map@1:" in line:
         map1 = line[7:]
-        if flag_1_1_write:
+        if flag_1_1_param_write and flag_1_1:
             f1.write(map1+"\n")
-            flag_1_1_write = False
+            flag_1_1_param_write = False
+            flag_1_1 = False
+            flag_latent_factor = True
         else:
-            f1.write(map1+" ")
-            if flag_0_1_write:
-                flag_0_1_write = False
+            if flag_0_1_param_write and flag_0_1:
+                f1.write(map1+" ")
+                flag_0_1_param_write = False
+                flag_0_1 = False
             else:
-                flag_1_0_write = False
+                if flag_1_0_param_write and flag_1_0:
+                    f1.write(map1+" ")
+                    flag_1_0_param_write = False
+                    flag_1_0 = False
 
 
 
