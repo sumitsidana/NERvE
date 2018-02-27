6a89ba3fc079cef958c5d6dc802c2db06f302f6f
sumit <sumit.sidana@imag.fr>
2018-02-27 20:24:37 +0100

    run recnet batches on kasandr

--- recnet_kasandr_batches.sh
+++ recnet_kasandr_batches.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p /data/sidana/recnet_draft/kasandr/recnet_batches/vectors
+mkdir -p /data/sidana/recnet/recnet_batches/kasandr/vectors
 
 #end_lf=20
 #reg_params="0.0001 0.001 0.005 0.01 0.05"
@@ -13,37 +13,37 @@ num_batches="1000 2000 3000 4000 5000 6000 7000 8000 9000 10000"
 
 for batch_num in $num_batches; do
 
-            echo -e "number of batches: $batch_num " >> /data/sidana/recnet_draft/kasandr/recnet_batches/results
+            echo -e "number of batches: $batch_num " >> /data/sidana/recnet/recnet_batches/kasandr/results
 
-            python3 train_test_split_batches.py kasandr 0 1 $batch_num
+            python3 train_test_split_batches.py kasandr 0 1 $batch_num 1 16 0.05
 
-            echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 01 one >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 01 five>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 01 ten >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
+            echo -e "alpha: 0, beta: 1" >>/data/sidana/recnet/recnet_batches/kasandr/results
+            echo -e -n "map@1: ">>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 01 one >>/data/sidana/recnet/recnet_batches/kasandr/results
+            echo -e -n "map@5: ">>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 01 five>>/data/sidana/recnet/recnet_batches/kasandr/results
+            echo -e -n "map@10:" >>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 01 ten >>/data/sidana/recnet/recnet_batches/kasandr/results
 
-            python3 train_test_split_batches.py kasandr 1 0 $batch_num
+            python3 train_test_split_batches.py kasandr 1 0 $batch_num 19 64 0.0001
 
-            echo -e "alpha: 1, beta: 0" >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 10 one>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 10 five>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 10 ten>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
+            echo -e "alpha: 1, beta: 0" >>/data/sidana/recnet/recnet_batches/kasandr/results
+            echo -e -n "map@1: ">>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 10 one>>/data/sidana/recnet/recnet_batches/kasandr/results
+            echo -e -n "map@5: ">>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 10 five>>/data/sidana/recnet/recnet_batches/kasandr/results
+            echo -e -n "map@10:" >>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 10 ten>>/data/sidana/recnet/recnet_batches/kasandr/results
 
-            python3 train_test_split_batches.py kasandr 1 1 $batch_num
+            python3 train_test_split_batches.py kasandr 1 1 $batch_num 18 64 0.005
 
-           echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-           echo -e -n "map@1: ">>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 11 one>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-           echo -e -n "map@5: ">>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 11 five>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-           echo -e -n "map@10:" >>/data/sidana/recnet_draft/kasandr/recnet_batches/results
-            ./writerelevancevectortrain_test_split_batches.sh kasandr 11 ten>>/data/sidana/recnet_draft/kasandr/recnet_batches/results
+           echo -e "alpha: 1, beta: 1" >>/data/sidana/recnet/recnet_batches/kasandr/results
+           echo -e -n "map@1: ">>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 11 one>>/data/sidana/recnet/recnet_batches/kasandr/results
+           echo -e -n "map@5: ">>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 11 five>>/data/sidana/recnet/recnet_batches/kasandr/results
+           echo -e -n "map@10:" >>/data/sidana/recnet/recnet_batches/kasandr/results
+            ./writerelevancevectortrain_test_split_batches.sh kasandr 11 ten>>/data/sidana/recnet/recnet_batches/kasandr/results
         done
 #    done
 #done
