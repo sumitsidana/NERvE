d66bc84531e7e78f0a6319f22f3b4cc84b176e03
sumit <sumit.sidana@imag.fr>
2018-02-27 12:08:42 +0100

    turn flag false on all occasions

--- parse_results_embeddings.py
+++ parse_results_embeddings.py
@@ -1,20 +1,14 @@
-# with open('results') as infile, open('results_output', 'w') as outfile:
-#     for line in infile:
-#         if not line.strip(): continue  # skip the empty line
-#         outfile.write(line)  # non-empty line. Write it to output
-	
-# f = open('results_output', 'r')
 import sys
 f = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/results', 'r')
 f1 = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/embeddings_'+sys.argv[1], 'w')
 
 flag_latent_factor = True
 
-flag_0_1 = False
+flag_0_1 = False #for alpha 0 beta 1 type line
 flag_1_0 = False
 flag_1_1 = False
 
-flag_0_1_param_write=False
+flag_0_1_param_write=False # for best parameters
 flag_1_0_param_write = False
 flag_1_1_param_write = False
 
@@ -34,12 +28,18 @@ for line in f:
 
     if line == "alpha: 0, beta: 1":
         flag_0_1 = True
+        flag_1_0 = False
+        flag_1_1 = False
 
     if line == "alpha: 1, beta: 0":
         flag_1_0 = True
+        flag_0_1 = False
+        flag_1_1 = False
 
     if line == "alpha: 1, beta: 1":
         flag_1_1 = True
+        flag_0_1 = False
+        flag_1_0 = False
 
     if "Latent Factor" in line:
         index = line.index(" ",15)
