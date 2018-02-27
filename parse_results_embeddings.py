2f198878f6ab36d51ebf87533581b13ec94cb867
sumit <sumit.sidana@imag.fr>
2018-02-27 09:46:06 +0100

    re-compute embeddings figure

--- parse_results_embeddings.py
+++ parse_results_embeddings.py
@@ -5,8 +5,8 @@
 	
 # f = open('results_output', 'r')
 import sys
-f = open('/data/sidana/recnet_draft/param_tune/'+sys.argv[1]+'/recnet/results', 'r')
-f1 = open('/data/sidana/recnet_draft/param_tune/'+sys.argv[1]+'/recnet/embeddings_'+sys.argv[1], 'w')
+f = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/results', 'r')
+f1 = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/embeddings_'+sys.argv[1], 'w')
 
 flag_0_1 = False
 flag_1_0 = False
