06d713d615a81e2420ec1a56508b399e0d421d1a
sumit <sumit.sidana@imag.fr>
2018-02-27 11:33:42 +0100

    turn flag false on all occasions

--- parse_results_embeddings.py
+++ parse_results_embeddings.py
@@ -8,7 +8,7 @@ import sys
 f = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/results', 'r')
 f1 = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/embeddings_'+sys.argv[1], 'w')
 
-flag_latent_factor = False
+flag_latent_factor = True
 
 flag_0_1 = False
 flag_1_0 = False
@@ -22,23 +22,24 @@ for line in f:
 
     line = line.strip('\n')
 
+    if "Regularization: 0.005 Hidden Units: 64" in line:
+        flag_0_1_param_write = True
+
+    if "Regularization: 0.05 Hidden Units: 32" in line:
+        flag_1_0_param_write = True
+
+    if "Regularization: 0.005 Hidden Units: 16" in line:
+        flag_1_1_param_write = True
+
+
     if line == "alpha: 0, beta: 1":
         flag_0_1 = True
-        flag_1_1 = False
-        flag_1_0 = False
-        continue
 
     if line == "alpha: 1, beta: 0":
-        flag_0_1 = False
-        flag_1_1 = False
         flag_1_0 = True
-        continue
 
     if line == "alpha: 1, beta: 1":
-        flag_0_1 = False
         flag_1_1 = True
-        flag_1_0 = False
-        continue
 
     if "Latent Factor" in line:
         index = line.index(" ",15)
@@ -48,16 +49,6 @@ for line in f:
             flag_latent_factor = False
             # need to turn it into True after writing results of 1 1
 
-
-
-    if "Regularization: 0.005 Hidden Units: 64" in line:
-        flag_0_1_param_write = True
-
-    if "Regularization: 0.05 Hidden Units: 32" in line:
-        flag_1_0_param_write = True
-
-    if "Regularization: 0.005 Hidden Units: 16" in line:
-        flag_1_1_param_write = True
 
 
 
