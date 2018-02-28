7e4891f83e7bde24d3b7702a58e4afa239b88829
sumit <sumit.sidana@imag.fr>
2018-02-28 15:01:18 +0100

    alpha work with best parameters

--- writerelevancevectortrain_test_split_alpha.sh
+++ writerelevancevectortrain_test_split_alpha.sh
@@ -1,6 +1,6 @@
 LANG=en_US.utf8
 cd java/
-mkdir -p /data/sidana/recnet_draft/recnet_alpha/$1/$2/rv/
+mkdir -p /data/sidana/recnet/recnet_alpha/$1/$2/rv/
 javac -cp binaries/commons-lang3-3.5.jar  preProcess/ConvertIntoRelVecGeneralized_update.java preProcess/InputOutput.java
 if [ $2 == "one" ]
 then
@@ -11,6 +11,6 @@ then
 else
    rank=10
 fi
-java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet_draft/recnet_alpha/$1/vectors/gt_$1 /data/sidana/recnet_draft/recnet_alpha/$1/vectors/pr_$1 /data/sidana/recnet_draft/recnet_alpha/$1/$2/rv/relevanceVector_$1 $rank
+java -cp . preProcess.ConvertIntoRelVecGeneralized_update /data/sidana/recnet/recnet_alpha/$1/vectors/gt_$1 /data/sidana/recnet/recnet_alpha/$1/vectors/pr_$1 /data/sidana/recnet/recnet_alpha/$1/$2/rv/relevanceVector_$1 $rank
 cd - >> uselessfile
-python3 compOfflineEvalMetrics_len${rank}_batches.py /data/sidana/recnet_draft/recnet_alpha/$1/$2 "$1"
+python3 compOfflineEvalMetrics_len${rank}_batches.py /data/sidana/recnet/recnet_alpha/$1/$2 "$1"
