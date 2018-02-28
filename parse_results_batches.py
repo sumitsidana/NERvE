08ebf6cd81e23fb0d554a3e5e26ca52f5fe46e13
sumit <sumit.sidana@imag.fr>
2018-02-28 10:41:18 +0100

    parse results for batches

--- parse_results_batches.py
+++ parse_results_batches.py
@@ -5,8 +5,8 @@
 	
 # f = open('results_output', 'r')
 import sys
-f = open('/data/sidana/recnet_draft/'+sys.argv[1]+'/recnet_batches/results', 'r')
-f1 = open('/data/sidana/recnet_draft/'+sys.argv[1]+'/recnet_batches/batches_'+sys.argv[1], 'w')
+f = open('/data/sidana/recnet/'+sys.argv[1]+'/recnet_batches/results', 'r')
+f1 = open('/data/sidana/recnet/'+sys.argv[1]+'/recnet_batches/batches_'+sys.argv[1], 'w')
 
 flag_0_1 = False
 flag_1_0 = False
