c1060a4947f2a83d4bf95a16f58329b622d6f26b
sumit <sumit.sidana@imag.fr>
2018-02-28 15:24:50 +0100

    alpha work with best parameters

--- recnet_kasandr_alpha.sh
+++ recnet_kasandr_alpha.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p /data/sidana/recnet_draft/kasandr/recnet_alpha/vectors
+mkdir -p /data/sidana/recnet/recnet_alpha/kasandr/vectors
 
 #end_lf=20
 #reg_params="0.0001 0.001 0.005 0.01 0.05"
@@ -13,16 +13,16 @@ alpha_values="0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
 
 for alpha_value in $alpha_values; do
 
-            echo -e "alpha_value: $alpha_value " >> /data/sidana/recnet_draft/kasandr/recnet_alpha/results
+            echo -e "alpha_value: $alpha_value " >> /data/sidana/recnet/recnet_alpha/kasandr/results
             beta_value="$(echo 1.0 - $alpha_value|bc)"
 
             python3 train_test_split_alpha.py kasandr $alpha_value $beta_value 18 0.005 64
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/kasandr/recnet_alpha/results
-            ./writerelevancevectortrain_test_split_alpha.sh kasandr one >>/data/sidana/recnet_draft/kasandr/recnet_alpha/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/kasandr/recnet_alpha/results
-            ./writerelevancevectortrain_test_split_alpha.sh kasandr five>>/data/sidana/recnet_draft/kasandr/recnet_alpha/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/kasandr/recnet_alpha/results
-            ./writerelevancevectortrain_test_split_alpha.sh kasandr ten >>/data/sidana/recnet_draft/kasandr/recnet_alpha/results
+            echo -e -n "map@1: ">>/data/sidana/recnet/recnet_alpha/kasandr/results
+            ./writerelevancevectortrain_test_split_alpha.sh kasandr one >>/data/sidana/recnet/recnet_alpha/kasandr/results
+            echo -e -n "map@5: ">>/data/sidana/recnet/recnet_alpha/kasandr/results
+            ./writerelevancevectortrain_test_split_alpha.sh kasandr five>>/data/sidana/recnet/recnet_alpha/kasandr/results
+            echo -e -n "map@10:" >>/data/sidana/recnet/recnet_alpha/kasandr/results
+            ./writerelevancevectortrain_test_split_alpha.sh kasandr ten >>/data/sidana/recnet/recnet_alpha/kasandr/results
 
         done
 #    done
