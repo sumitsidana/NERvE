8fb72d668886573cd9f94f3700f3ae47ac0c7f7c
sumit <sumit.sidana@imag.fr>
2018-02-28 15:22:57 +0100

    alpha work with best parameters

--- recnet_ml1m_alpha.sh
+++ recnet_ml1m_alpha.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p /data/sidana/recnet_draft/ml1m/recnet_alpha/vectors
+mkdir -p /data/sidana/recnet/recnet_alpha/ml1m/vectors
 
 #end_lf=20
 #reg_params="0.0001 0.001 0.005 0.01 0.05"
@@ -14,16 +14,16 @@ alpha_values="0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
 for alpha_value in $alpha_values; do
 
 
-            echo -e "alpha_value: $alpha_value " >> /data/sidana/recnet_draft/ml1m/recnet_alpha/results
+            echo -e "alpha_value: $alpha_value " >> /data/sidana/recnet/recnet_alpha/ml1m/results
             beta_value="$(echo 1.0 - $alpha_value|bc)"
 
             python3 train_test_split_alpha.py ml1m $alpha_value $beta_value 1 0.001 32
-            echo -e -n "map@1: ">>/data/sidana/recnet_draft/ml1m/recnet_alpha/results
-            ./writerelevancevectortrain_test_split_alpha.sh ml1m one >>/data/sidana/recnet_draft/ml1m/recnet_alpha/results
-            echo -e -n "map@5: ">>/data/sidana/recnet_draft/ml1m/recnet_alpha/results
-            ./writerelevancevectortrain_test_split_alpha.sh ml1m five>>/data/sidana/recnet_draft/ml1m/recnet_alpha/results
-            echo -e -n "map@10:" >>/data/sidana/recnet_draft/ml1m/recnet_alpha/results
-            ./writerelevancevectortrain_test_split_alpha.sh ml1m ten >>/data/sidana/recnet_draft/ml1m/recnet_alpha/results
+            echo -e -n "map@1: ">>/data/sidana/recnet/recnet_alpha/ml1m/results
+            ./writerelevancevectortrain_test_split_alpha.sh ml1m one >>/data/sidana/recnet/recnet_alpha/ml1m/results
+            echo -e -n "map@5: ">>/data/sidana/recnet/recnet_alpha/ml1m/results
+            ./writerelevancevectortrain_test_split_alpha.sh ml1m five>>/data/sidana/recnet/recnet_alpha/ml1m/results
+            echo -e -n "map@10:" >>/data/sidana/recnet/recnet_alpha/ml1m/results
+            ./writerelevancevectortrain_test_split_alpha.sh ml1m ten >>/data/sidana/recnet/recnet_alpha/ml1m/results
 
         done
 #    done
