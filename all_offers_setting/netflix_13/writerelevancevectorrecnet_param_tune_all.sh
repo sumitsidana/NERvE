2b6b190a4e795d0be905415019eaab4d3ae51554
sumit <sumit.sidana@imag.fr>
2018-03-12 14:18:03 +0100

    embedding 13

--- all_offers_setting/netflix_13/writerelevancevectorrecnet_param_tune_all.sh
+++ all_offers_setting/netflix_13/writerelevancevectorrecnet_param_tune_all.sh
@@ -1,6 +1,6 @@
 LANG=en_US.utf8
 
-mkdir -p /data/sidana/recnet/all_test_param_tune/$1/recnet_all/10_15/$3/rv/
+mkdir -p /data/sidana/recnet/all_test_param_tune/$1/recnet_all/13/$3/rv/
 javac  ConvertIntoRelVecGeneralized_update.java
 if [ $3 == "one" ]
 then
@@ -11,6 +11,6 @@ then
 else
    rank=10
 fi
-java -cp . ConvertIntoRelVecGeneralized_update /data/sidana/recnet/all_test_param_tune/$1/recnet_all/10_15/vectors/gt_$1_$2 /data/sidana/recnet/all_test_param_tune/$1/recnet_all/10_15/vectors/pr_$1_$2 /data/sidana/recnet/all_test_param_tune/$1/recnet_all/10_15/$3/rv/relevanceVector_$1_$2 $rank
+java -cp . ConvertIntoRelVecGeneralized_update /data/sidana/recnet/all_test_param_tune/$1/recnet_all/13/vectors/gt_$1_$2 /data/sidana/recnet/all_test_param_tune/$1/recnet_all/13/vectors/pr_$1_$2 /data/sidana/recnet/all_test_param_tune/$1/recnet_all/13/$3/rv/relevanceVector_$1_$2 $rank
 
-python3 compOfflineEvalMetrics_len${rank}_param_tune.py /data/sidana/recnet/all_test_param_tune/$1/recnet_all/10_15/$3 "$1_$2"
+python3 compOfflineEvalMetrics_len${rank}_param_tune.py /data/sidana/recnet/all_test_param_tune/$1/recnet_all/13/$3 "$1_$2"
