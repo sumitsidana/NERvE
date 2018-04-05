7f5ce17d00865b95fda73465f4f62167c5e2a8eb
sumit <sumit.sidana@imag.fr>
2018-04-05 16:18:03 +0200

    commit every thing

--- figs/plotMap_emb_ml1m.p
+++ figs/plotMap_emb_ml1m.p
@@ -13,12 +13,12 @@ set offset -0.3,-0.3,0,0
 set style line 1 lc rgb '#CC0000' lt 1 lw 2 pt 7 ps 1.5   # --- blue
 set style line 2 lc rgb '#009900' lt 1 lw 2 pt 5 ps 1.5   # --- blue
 set style line 3 lc rgb '#0000CC' lt 1 lw 2 pt 5 ps 1.5   # --- blue
-set output "PlotMap_KASANDR.eps";
+set output "PlotMap_ML1M.eps";
 set datafile separator " "
 set yrange [0.65:1]
 set xrange [-1:20]
 set key spacing 2
 set key top right
-plot  'embeddings_kasandr' using ($2):xticlabels(1) with linespoints ls 1 title 'Recnet_c', \
- '' using ($4):xticlabels(1) with linespoints ls 2 title 'Recnet_p', \
+plot  'embeddings_ml1m' using ($4):xticlabels(1) with linespoints ls 1 title 'Recnet_c', \
+ '' using ($2):xticlabels(1) with linespoints ls 2 title 'Recnet_p', \
  '' using ($3):xticlabels(1) with linespoints ls 3 title 'Recnet_{c,p}'
---
+++ parse_results_embeddings_ml100k.py
@@ -0,0 +1,82 @@
+import sys
+f = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/results', 'r')
+f1 = open('/data/sidana/recnet/param_tune/'+sys.argv[1]+'/recnet/embeddings_'+sys.argv[1], 'w')
+
+flag_latent_factor = True
+
+flag_0_1 = False #for alpha 0 beta 1 type line
+flag_1_0 = False
+flag_1_1 = False
+
+flag_0_1_param_write = False # for best parameters
+flag_1_0_param_write = False
+flag_1_1_param_write = False
+
+for line in f:
+
+    line = line.strip('\n')
+
+    if "Latent Factor" in line:
+        index = line.index(" ",15)
+        latent_factor = line[15:index]
+        if flag_latent_factor:
+            f1.write(latent_factor + " ")
+            flag_latent_factor = False
+
+    if "Regularization: 0.005 Hidden Units: 64" in line:
+        flag_0_1_param_write = True
+
+
+    if "Regularization: 0.05 Hidden Units: 32" in line:
+        flag_1_0_param_write = True
+
+
+    if "Regularization: 0.005 Hidden Units: 16" in line:
+        flag_1_1_param_write = True
+
+
+    if line == "alpha: 0, beta: 1":
+        flag_0_1 = True
+        flag_1_0 = False
+        flag_1_1 = False
+
+    if line == "alpha: 1, beta: 0":
+        flag_1_0 = True
+        flag_0_1 = False
+        flag_1_1 = False
+
+    if line == "alpha: 1, beta: 1":
+        flag_1_1 = True
+        flag_0_1 = False
+        flag_1_0 = False
+
+            # need to turn it into True after writing results of 1 1
+
+
+
+
+    if "map@1:" in line:
+        map1 = line[7:]
+        if flag_1_1_param_write and flag_1_1:
+            f1.write(map1+" ")
+            flag_1_1_param_write = False
+            flag_1_1 = False
+        else:
+            if flag_0_1_param_write and flag_0_1:
+                f1.write(map1+" ")
+                flag_0_1_param_write = False
+                flag_0_1 = False
+            else:
+                if flag_1_0_param_write and flag_1_0:
+                    f1.write(map1+"\n")
+                    flag_1_0_param_write = False
+                    flag_1_0 = False
+
+    if "Regularization: 0.05 Hidden Units: 64" in line:
+        flag_latent_factor = True # This is not being set properly
+
+
+
+
+f1.close()
+f.close()
