04ef36005497393fa770474867124551d2f3f45a
sumit <sumit.sidana@imag.fr>
2018-09-17 12:15:35 +0200

    pandor cofacor

---
+++ cofactor_pandor.sh
@@ -0,0 +1,15 @@
+cd ~/cofactor/src
+LANG=en_US.utf8
+echo "preprocessing"
+python preprocess.py "/data/sidana/recnet_draft/pandor/cofactor"
+echo "running cofactor"
+python Cofactorization.py "/data/sidana/recnet_draft/pandor/cofactor/pro"
+cd ~/nnmf_ranking/
+./writegroundtruthforcofactor.sh pandor
+./writepredictorforcofator.sh pandor
+./writerelevancevectorcofactor.sh pandor one
+./writerelevancevectorcofactor.sh pandor five
+./writerelevancevectorcofactor.sh pandor ten
+
+# For all items
+# python Cofactorization.py /data/sidana/recnet_draft/kasandr/cofactor_all/pro/ and keep changing "k" in Cofactorization.py
