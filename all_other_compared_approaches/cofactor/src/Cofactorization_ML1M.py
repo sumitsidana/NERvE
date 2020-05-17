4b46cc99aa7b10ea7a77c0973fb69df9eb4598c5
sumitsidana <sidana.sumit@gmail.com>
2020-05-17 20:46:13 +0200

    cofactor

---
+++ all_other_compared_approaches/cofactor/src/Cofactorization_ML1M.py
@@ -0,0 +1,336 @@
+
+# coding: utf-8
+
+# # Fit CoFactor model to the binarized ML20M
+
+# In[1]:
+
+import itertools
+import glob
+import os
+import sys
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import pandas as pd
+from scipy import sparse
+import seaborn as sns
+sns.set(context="paper", font_scale=1.5, rc={"lines.linewidth": 2}, font='DejaVu Serif')
+
+
+# In[2]:
+
+import cofacto
+import rec_eval
+
+
+# ### Construct the positive pairwise mutual information (PPMI) matrix
+
+# Change this to wherever you saved the pre-processed data following [this notebook](./preprocess_ML20M.ipynb).
+
+# In[3]:
+
+DATA_DIR = sys.argv[1]
+
+
+# In[4]:
+
+unique_uid = list()
+with open(os.path.join(DATA_DIR, 'unique_uid.txt'), 'r') as f:
+    for line in f:
+        unique_uid.append(line.strip())
+    
+unique_sid = list()
+with open(os.path.join(DATA_DIR, 'unique_sid.txt'), 'r') as f:
+    for line in f:
+        unique_sid.append(line.strip())
+
+
+# In[5]:
+
+n_items = len(unique_sid)
+n_users = len(unique_uid)
+
+print n_users, n_items
+
+
+# In[6]:
+
+def load_data(csv_file, shape=(n_users, n_items)):
+    tp = pd.read_csv(csv_file)
+    timestamps, rows, cols = np.array(tp['timestamp']), np.array(tp['uid']), np.array(tp['sid'])
+    seq = np.concatenate((rows[:, None], cols[:, None], np.ones((rows.size, 1), dtype='int'), timestamps[:, None]), axis=1)
+    data = sparse.csr_matrix((np.ones_like(rows), (rows, cols)), dtype=np.int16, shape=shape)
+    return data, seq
+
+
+# In[7]:
+
+train_data, train_raw = load_data(os.path.join(DATA_DIR, 'train.csv'))
+
+
+# In[8]:
+
+watches_per_movie = np.asarray(train_data.astype('int64').sum(axis=0)).ravel()
+
+
+# In[9]:
+
+print("The mean (median) watches per movie is %d (%d)" % (watches_per_movie.mean(), np.median(watches_per_movie)))
+
+
+# In[10]:
+
+user_activity = np.asarray(train_data.sum(axis=1)).ravel()
+
+
+# In[11]:
+
+print("The mean (median) movies each user wathced is %d (%d)" % (user_activity.mean(), np.median(user_activity)))
+
+
+# In[12]:
+
+vad_data, vad_raw = load_data(os.path.join(DATA_DIR, 'validation.csv'))
+
+
+# In[13]:
+
+plt.semilogx(1 + np.arange(n_users), -np.sort(-user_activity), 'o')
+plt.ylabel('Number of items that this user clicked on')
+plt.xlabel('User rank by number of consumed items')
+pass
+
+
+# In[14]:
+
+plt.semilogx(1 + np.arange(n_items), -np.sort(-watches_per_movie), 'o')
+plt.ylabel('Number of users who watched this movie')
+plt.xlabel('Movie rank by number of watches')
+pass
+
+
+# ### Generate co-occurrence matrix based on the user's entire watching history
+
+# In[17]:
+
+def _coord_batch(lo, hi, train_data):
+    rows = []
+    cols = []
+    for u in xrange(lo, hi):
+        for w, c in itertools.permutations(train_data[u].nonzero()[1], 2):
+            rows.append(w)
+            cols.append(c)
+    np.save(os.path.join(DATA_DIR, 'coo_%d_%d.npy' % (lo, hi)),
+            np.concatenate([np.array(rows)[:, None], np.array(cols)[:, None]], axis=1))
+    pass
+
+
+# In[18]:
+
+from joblib import Parallel, delayed
+
+batch_size = 5000
+
+start_idx = range(0, n_users, batch_size)
+end_idx = start_idx[1:] + [n_users]
+
+Parallel(n_jobs=8)(delayed(_coord_batch)(lo, hi, train_data) for lo, hi in zip(start_idx, end_idx))
+pass
+
+
+# In[19]:
+
+X = sparse.csr_matrix((n_items, n_items), dtype='float32')
+
+for lo, hi in zip(start_idx, end_idx):
+    coords = np.load(os.path.join(DATA_DIR, 'coo_%d_%d.npy' % (lo, hi)))
+
+    rows = coords[:, 0]
+    cols = coords[:, 1]
+
+    tmp = sparse.coo_matrix((np.ones_like(rows), (rows, cols)), shape=(n_items, n_items), dtype='float32').tocsr()
+    X = X + tmp
+
+    print("User %d to %d finished" % (lo, hi))
+    sys.stdout.flush()
+
+
+# Note: Don't forget to delete all the temporary coo_LO_HI.npy files
+
+# In[20]:
+
+np.save(os.path.join(DATA_DIR, 'coordinate_co_binary_data.npy'), X.data)
+np.save(os.path.join(DATA_DIR, 'coordinate_co_binary_indices.npy'), X.indices)
+np.save(os.path.join(DATA_DIR, 'coordinate_co_binary_indptr.npy'), X.indptr)
+
+
+# In[21]:
+
+float(X.nnz) / np.prod(X.shape)
+
+
+# ### Or load the pre-saved co-occurrence matrix
+
+# In[15]:
+
+# or co-occurrence matrix from the entire user history
+dir_predix = DATA_DIR
+
+
+# In[16]:
+
+data = np.load(os.path.join(dir_predix, 'coordinate_co_binary_data.npy'))
+indices = np.load(os.path.join(dir_predix, 'coordinate_co_binary_indices.npy'))
+indptr = np.load(os.path.join(dir_predix, 'coordinate_co_binary_indptr.npy'))
+
+
+# In[17]:
+
+X = sparse.csr_matrix((data, indices, indptr), shape=(n_items, n_items))
+
+
+# In[18]:
+
+float(X.nnz) / np.prod(X.shape)
+
+
+# In[19]:
+
+def get_row(Y, i):
+    lo, hi = Y.indptr[i], Y.indptr[i + 1]
+    return lo, hi, Y.data[lo:hi], Y.indices[lo:hi]
+
+
+# In[20]:
+
+count = np.asarray(X.sum(axis=1)).ravel()
+
+
+# In[21]:
+
+n_pairs = X.data.sum()
+
+
+# ### Construct the SPPMI matrix
+
+# In[22]:
+
+M = X.copy()
+
+for i in xrange(n_items):
+    lo, hi, d, idx = get_row(M, i)
+    M.data[lo:hi] = np.log(d * n_pairs / (count[i] * count[idx]))
+
+
+# In[23]:
+
+M.data[M.data < 0] = 0
+M.eliminate_zeros()
+
+
+# In[24]:
+
+print float(M.nnz) / np.prod(M.shape)
+
+
+# Now $M$ is the PPMI matrix. Depending on the number of negative examples $k$, we can obtain the shifted PPMI matrix as $\max(M_{wc} - \log k, 0)$
+
+# In[25]:
+
+# number of negative samples
+k_ns = 1
+
+M_ns = M.copy()
+
+if k_ns > 1:
+    offset = np.log(k_ns)
+else:
+    offset = 0.
+    
+M_ns.data -= offset
+M_ns.data[M_ns.data < 0] = 0
+M_ns.eliminate_zeros()
+
+
+# In[26]:
+
+plt.hist(M_ns.data, bins=50)
+plt.yscale('log')
+pass
+
+
+# In[27]:
+
+float(M_ns.nnz) / np.prod(M_ns.shape)
+
+
+# ### Train the model
+
+# In[28]:
+
+scale = 0.03
+
+n_components = 100
+max_iter = 20
+n_jobs = 8
+lam_theta = lam_beta = 1e-5 * scale
+lam_gamma = 1e-5
+c0 = 1. * scale
+c1 = 10. * scale
+
+save_dir = os.path.join(DATA_DIR, 'ML20M_ns%d_scale%1.2E' % (k_ns, scale))
+
+
+# In[29]:
+
+reload(cofacto)
+coder = cofacto.CoFacto(n_components=n_components, max_iter=max_iter, batch_size=1000, init_std=0.01, n_jobs=n_jobs, 
+                        random_state=98765, save_params=True, save_dir=save_dir, early_stopping=True, verbose=True, 
+                        lam_theta=lam_theta, lam_beta=lam_beta, lam_gamma=lam_gamma, c0=c0, c1=c1)
+
+
+# In[30]:
+
+coder.fit(train_data, M_ns, vad_data=vad_data, batch_users=5000, k=100)
+
+
+# In[31]:
+
+test_data, _ = load_data(os.path.join(DATA_DIR, 'test.csv'))
+test_data.data = np.ones_like(test_data.data)
+
+
+# In[32]:
+
+n_params = len(glob.glob(os.path.join(save_dir, '*.npz')))
+
+params = np.load(os.path.join(save_dir, 'CoFacto_K%d_iter%d.npz' % (n_components, n_params - 1)))
+U, V = params['U'], params['V']
+
+
+#write file for map
+
+# In[33]:
+#user_idx = rec_eval.user_idx_generator(test_data.shape[1], test_data)
+#batch_user=user_idx.stop-user_idx.start
+#X_pred = rec_eval._make_prediction(train_data,  U, V, user_idx, batch_user,  vad_data=test_data)
+#print 'Test Recall@20: %.4f' % rec_eval.recall_at_k(train_data, test_data, U, V, k=20, vad_data=vad_data)
+#print 'Test Recall@50: %.4f' % rec_eval.recall_at_k(train_data, test_data, U, V, k=50, vad_data=vad_data)
+#print 'Test NDCG@10: %.4f' % rec_eval.normalized_dcg_at_k(train_data, test_data, U, V, k=10, vad_data=vad_data)
+print 'Test MAP@10: %.4f' % rec_eval.map_at_k(train_data, test_data, U, V, k=10, vad_data=vad_data)
+
+
+# In[34]:
+
+np.savez('CoFactor_K100_ML20M.npz', U=U, V=V)
+
+
+# In[ ]:
+
+
+
---
+++ all_other_compared_approaches/cofactor/src/preprocess_netflix.py
@@ -0,0 +1,298 @@
+
+# coding: utf-8
+
+# # Preprocess MovieLens-20M 
+
+# In[1]:
+
+import datetime
+import json
+import os
+import time
+
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+import pandas as pd
+import scipy.sparse
+
+import seaborn as sns
+sns.set(context="paper", font_scale=1.5, rc={"lines.linewidth": 2}, font='DejaVu Serif')
+
+
+# In[2]:
+
+DATA_DIR = '/data/sidana/nnmf_ranking/netflix/'
+
+
+# In[3]:
+
+def timestamp_to_date(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
+# In[4]:
+
+raw_data = pd.read_csv(os.path.join(DATA_DIR, 'ratings.csv'), header=0)
+
+
+# In[5]:
+
+# binarize the data (only keep ratings >= 4)
+raw_data = raw_data[raw_data['rating'] > 3.5]
+
+
+# In[6]:
+
+# sort the raw data accorindg to timestamp
+raw_data = raw_data.sort_index(by=['timestamp'])
+
+
+# In[7]:
+
+raw_data
+
+
+# In[8]:
+
+tstamp = np.array(raw_data['timestamp'])
+
+
+# In[9]:
+
+print("Time span of the dataset: From %s to %s" % 
+      (timestamp_to_date(np.min(tstamp)), timestamp_to_date(np.max(tstamp))))
+
+
+# In[10]:
+
+# apparently the timestamps are ordered, check to make sure
+
+for i in xrange(tstamp.size - 1):
+    if tstamp[i] > tstamp[i + 1]:
+        print("not ordered")
+
+
+# Confirmed the timestamps are ordered
+
+# In[11]:
+
+plt.hist(tstamp, bins=50)
+xticks = np.linspace(tstamp[0], tstamp[-1], 10)
+plt.xticks(xticks, map(lambda x: timestamp_to_date(x)[:7], xticks), rotation=90)
+pass
+
+
+# Now we select the data from 1995-01-01 to the last day as the dataset (i.e., all the dataset)
+
+# In[12]:
+
+start_t = time.mktime(datetime.datetime.strptime("1995-01-01", "%Y-%m-%d").timetuple())
+
+
+# In[42]:
+
+raw_data = raw_data[raw_data['timestamp'] >= start_t]
+
+
+# Take the first 80% of the data as train and validation set
+
+# In[96]:
+
+tr_vd_raw_data = raw_data[:int(0.8 * raw_data.shape[0])]
+
+
+# In[97]:
+
+def get_count(tp, id):
+    playcount_groupbyid = tp[[id]].groupby(id, as_index=False)
+    count = playcount_groupbyid.size()
+    return count
+
+
+# In[98]:
+
+def filter_triplets(tp, min_uc=5, min_sc=0):
+    # Only keep the triplets for songs which were listened to by at least min_sc users. 
+    if min_sc > 0:
+        songcount = get_count(tp, 'movieId')
+        tp = tp[tp['movieId'].isin(songcount.index[songcount >= min_sc])]
+    
+    # Only keep the triplets for users who listened to at least min_uc songs
+    # After doing this, some of the songs will have less than min_uc users, but should only be a small proportion
+    if min_uc > 0:
+        usercount = get_count(tp, 'userId')
+        tp = tp[tp['userId'].isin(usercount.index[usercount >= min_uc])]
+    
+    # Update both usercount and songcount after filtering
+    usercount, songcount = get_count(tp, 'userId'), get_count(tp, 'movieId') 
+    return tp, usercount, songcount
+
+
+# In[99]:
+
+tr_vd_raw_data, user_activity, item_popularity = filter_triplets(tr_vd_raw_data)
+
+
+# In[100]:
+
+sparsity = 1. * tr_vd_raw_data.shape[0] / (user_activity.shape[0] * item_popularity.shape[0])
+
+print("After filtering, there are %d watching events from %d users and %d movies (sparsity: %.3f%%)" % 
+      (tr_vd_raw_data.shape[0], user_activity.shape[0], item_popularity.shape[0], sparsity * 100))
+
+
+# In[101]:
+
+unique_uid = user_activity.index
+unique_sid = item_popularity.index
+
+
+# In[102]:
+
+song2id = dict((sid, i) for (i, sid) in enumerate(unique_sid))
+user2id = dict((uid, i) for (i, uid) in enumerate(unique_uid))
+
+
+# In[103]:
+
+with open(os.path.join(DATA_DIR, 'pro', 'unique_uid.txt'), 'w') as f:
+    for uid in unique_uid:
+        f.write('%s\n' % uid)
+
+
+# In[104]:
+
+with open(os.path.join(DATA_DIR, 'pro', 'unique_sid.txt'), 'w') as f:
+    for sid in unique_sid:
+        f.write('%s\n' % sid)
+
+
+# Split 12.5% (10% of the total ratings) as validation set
+
+# In[105]:
+
+np.random.seed(13579)
+n_ratings = tr_vd_raw_data.shape[0]
+vad = np.random.choice(n_ratings, size=int(0.125 * n_ratings), replace=False)
+
+
+# In[106]:
+
+vad_idx = np.zeros(n_ratings, dtype=bool)
+vad_idx[vad] = True
+
+vad_raw_data = tr_vd_raw_data[vad_idx]
+train_raw_data = tr_vd_raw_data[~vad_idx]
+
+
+# Make sure there is no empty users/items
+
+# In[107]:
+
+print "There are total of %d unique users in the training set and %d unique users in the entire dataset" % (len(pd.unique(train_raw_data['userId'])), len(unique_uid))
+
+
+# In[108]:
+
+print "There are total of %d unique items in the training set and %d unique items in the entire dataset" % (len(pd.unique(train_raw_data['movieId'])), len(unique_sid))
+
+
+# In[109]:
+
+train_sid = set(pd.unique(train_raw_data['movieId']))
+
+
+# In[110]:
+
+left_sid = list()
+for i, sid in enumerate(unique_sid):
+    if sid not in train_sid:
+        left_sid.append(sid)
+
+
+# In[111]:
+
+move_idx = vad_raw_data['movieId'].isin(left_sid)
+
+
+# In[112]:
+
+train_raw_data = train_raw_data.append(vad_raw_data[move_idx])
+vad_raw_data = vad_raw_data[~move_idx]
+
+
+# In[113]:
+
+print "There are total of %d unique items in the training set and %d unique items in the entire dataset" % (len(pd.unique(train_raw_data['movieId'])), len(unique_sid))
+
+
+# For test data, only keep the users and items that appear in the training/validation sets
+
+# In[114]:
+
+test_raw_data = raw_data[int(0.8 * len(raw_data)):]
+
+
+# In[115]:
+
+test_raw_data = test_raw_data[test_raw_data['movieId'].isin(unique_sid)]
+test_raw_data = test_raw_data[test_raw_data['userId'].isin(unique_uid)]
+
+
+# In[116]:
+
+print len(train_raw_data), len(vad_raw_data), len(test_raw_data)
+
+
+# Basic data information: what's the timespan for train/test?
+
+# In[117]:
+
+train_timestamp = np.asarray(tr_vd_raw_data['timestamp'])
+print("train: from %s to %s" % (timestamp_to_date(train_timestamp[0]), 
+                                timestamp_to_date(train_timestamp[-1])))
+
+test_timestamp = np.asarray(test_raw_data['timestamp'])
+print("test: from %s to %s" % (timestamp_to_date(test_timestamp[0]), 
+                               timestamp_to_date(test_timestamp[-1])))
+
+
+# ### Numerize the data into (timestamp, user_index, item_index) format
+
+# In[118]:
+
+def numerize(tp):
+    uid = map(lambda x: user2id[x], tp['userId'])
+    sid = map(lambda x: song2id[x], tp['movieId'])
+    tp['uid'] = uid
+    tp['sid'] = sid
+    return tp[['timestamp', 'uid', 'sid']]
+
+
+# In[119]:
+
+train_data = numerize(train_raw_data)
+train_data.to_csv(os.path.join(DATA_DIR, 'pro', 'train.csv'), index=False)
+
+
+# In[120]:
+
+vad_data = numerize(vad_raw_data)
+vad_data.to_csv(os.path.join(DATA_DIR, 'pro', 'validation.csv'), index=False)
+
+
+# In[121]:
+
+test_data = numerize(test_raw_data)
+test_data.to_csv(os.path.join(DATA_DIR, 'pro', 'test.csv'), index=False)
+
+
+# In[ ]:
+
+
+
